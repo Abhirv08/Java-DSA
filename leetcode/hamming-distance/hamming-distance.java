3847663a6a329cf96c04ae434d570203dfa03eb5
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2021-12-31 14:26:20 +0530

    Update hamming-distance.java

--- leetcode/hamming-distance/hamming-distance.java
+++ leetcode/hamming-distance/hamming-distance.java
@@ -2,13 +2,6 @@ class Solution {
     public int hammingDistance(int x, int y) {
         int temp = x ^ y;
         int count = 0;
-        // while(temp > 0){
-        //     if((temp & 1) ==1){
-        //         count++;
-        //     }
-        //     temp = temp >> 1;
-        // }
-        
         while(temp > 0){
             temp = temp & (temp-1);
             count++;
