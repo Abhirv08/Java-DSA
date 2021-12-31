4b7aaa9545876ce703a503c1fb6e4fbeba3aa586
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2021-12-31 20:55:48 +0530

    Time: 235 ms (5.04%), Space: 40.2 MB (88.45%) - LeetHub

---
+++ set-mismatch/set-mismatch.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        Arrays.sort(nums);
+        int repeated_num =0;
+        int missing_num = 0;
+        for(int i= 1; i<=nums.length; i++){
+            int count = 0;
+            for(int j = 0; j<nums.length; j++){
+                if(nums[j] > i){
+                    break;
+                }else if(nums[j] == i){
+                    count++;
+                }
+            }
+            if(count == 0){
+                missing_num = i;
+            }else if(count == 2){
+                 repeated_num = i;
+            }
+            if( repeated_num>0 &&  missing_num>0 ){
+                break;
+            }
+        }
+        return new int[] {repeated_num, missing_num};
+    }
+}
