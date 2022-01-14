e95059a4c7bdf0f42f3e370dbec190300313942a
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-01-15 01:12:28 +0530

    Time: 4 ms (40.89%), Space: 39.2 MB (54.75%) - LeetHub

--- 67-add-binary/67-add-binary.java
+++ 67-add-binary/67-add-binary.java
@@ -1,74 +1,25 @@
 class Solution {
     static String addBinary(String a, String b) {
-        if(a.length() < b.length()){
-            String temp = b;
-            b = a;
-            a = temp;
-        }
-        int m = a.length();
-        int n = b.length();
-        int carry = 0;
-        int i = n-1;
-        String ans = "";
-        while (i >= 0) {
-            String p = a.substring(m-1, m);
-            String q = b.substring(i, i+1);
-            if(p.equals("1") && q.equals("1")){
-                if (a.length()==b.length() && i == 0){
-                    if (carry == 1){
-                        ans = 11 + ans;
-                    }else{
-                        ans = 10 + ans;
-                    }
-                }else{
-                    if (carry==1){
-                        ans = 1 + ans;
-                        carry = 1;
-                    }else{
-                        ans = 0 + ans;
-                        carry = 1;
-                    }
-                }
-            }else if(p.equals("0")&&q.equals("0")){
-                ans = (0+carry) + ans;
-                carry = 0;
-            }else{
-                if (carry == 1){
-                    ans = 0 + ans;
-                    carry = 1;
-                }else{
-                    ans = 1 + ans;
-                }
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int rem = 0;
+        StringBuilder result = new StringBuilder();
+        while (i >= 0 || j >= 0) {
+            if (i >= 0) {
+                rem += (a.charAt(i) - '0');
             }
-            i--;m--;
-        }
-        int j  = a.length()-n;
-        i = a.length() - n - 1;
-        while (j>0){
-            String p = a.substring(i, i+1);
-            if (i==0){
-                if (carry==1){
-                    ans = 10 + ans;
-                }else{
-                    ans = 1 + ans;
-                }
-            }else{
-                if (carry==1){
-                    if (p.equals("1")){
-                        ans = 0 + ans;
-                        carry = 1;
-                    }else{
-                        ans = 1 + ans;
-                        carry = 0;
-                    }
-                }else{
-                    ans = p + ans;
-                }
+            if (j >= 0) {
+                rem += (b.charAt(j) - '0');
             }
-            i--;j--;
+            result.append((rem % 2));
+            rem /= 2;
+            i--;
+            j--;
         }
-
-        return ans;
+        if (rem > 0) {
+            result.append(rem);
+        }
+        return result.reverse().toString();
     }
 
     
