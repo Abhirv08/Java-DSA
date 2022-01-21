ecc2f8c4975085ce6b1b69cf375279c755fcf946
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-01-22 00:22:37 +0530

    Time: 9 ms (71.13%), Space: 42.3 MB (63.58%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,10 +1,18 @@
 class Solution {
-    public int shipWithinDays(int[] weights, int days) {
-        int l = 1;
-        int r = 500*50000;
+    public int shipWithinDays(int[] wt, int days) {
+        int max = 0;
+            int sum = 0;
+            for(int i = 0; i < wt.length; i++){
+                if(wt[i] > max){
+                    max = wt[i];
+                }
+                sum += wt[i];
+            }
+        int l = max;
+        int r = sum;
         while(l < r){
             int mid = (l+r)/2;
-            if(isOk(mid, weights, days)){
+            if(isOk(mid, wt, days)){
                 r = mid;
             }else{
                 l = mid+1;
