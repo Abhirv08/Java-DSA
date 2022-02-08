0ff0b24b50b8c17142a64913048cdab720431f81
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-02-09 01:59:17 +0530

    Create Reorder_list.java

---
+++ doubt/Reorder_list.java
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/reorder-list/
+
+// Q. You are given the head of a singly linked-list. The list can be represented as:
+
+// L0 → L1 → … → Ln - 1 → Ln
+// Reorder the list to be on the following form:
+
+// L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+// You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+// Example: 
+// Input : [1,2,3,4,5,6,7,8,9]
+// Output: [1,9,2,8,3,7,4,6,5]
