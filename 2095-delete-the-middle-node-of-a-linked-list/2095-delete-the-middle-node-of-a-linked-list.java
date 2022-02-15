86ade372d635c61e8bab229fefc87a4ee11b7c93
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-02-15 21:34:50 +0530

    Time: 44 ms (6.36%), Space: 262 MB (5.01%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -11,18 +11,36 @@
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
         if(head.next == null) return null;
+        ArrayList<Integer> nodes = new ArrayList<>();
         ListNode temp = head;
-        int middle = size(head)/2;
-        int counter = 0;
-        ListNode prev= null, nextNode = null;
-        while(counter <= middle){
-            if(counter==middle-1){prev = temp;}
-            temp = temp.next;            
-            counter++;
+        while(temp != null){
+            nodes.add(temp.val);
+            temp = temp.next;
         }
-        nextNode = temp;
-        prev.next = nextNode;
+        nodes.remove((int)nodes.size()/2);
+        temp = new ListNode(nodes.get(0));
+        head = temp;
+        for(int i = 1; i < nodes.size(); i++){
+            temp.next = new ListNode(nodes.get(i));
+            temp = temp.next;
+        }
+        temp = null;
         return head;
+        
+        
+        // if(head.next == null) return null;
+        // ListNode temp = head;
+        // int middle = size(head)/2;
+        // int counter = 0;
+        // ListNode prev= null, nextNode = null;
+        // while(counter <= middle){
+        //     if(counter==middle-1){prev = temp;}
+        //     temp = temp.next;            
+        //     counter++;
+        // }
+        // nextNode = temp;
+        // prev.next = nextNode;
+        // return head;
     }
     
     static int size(ListNode head){
