bdd403cc0d6f67691a554c6c6a92c3d2597f8b18
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-03-05 03:41:08 +0530

    Time: 4 ms (10.98%), Space: 43.2 MB (5.48%) - LeetHub

--- 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.java
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.java
@@ -1,37 +1,35 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-//         Stack<String> st = new Stack<>();
-//         int i = 0;
-//         while(i < s.length()){
-//             if(s.substring(i, i+1).equals("(")){
-//                 st.push(")");
-//             }else{
-//                 if(!st.isEmpty() && s.substring(i, i+1).equals(st.peek())){
-//                     st.pop();
-//                 }else{
-//                     st.push("(");
-//                 }
-//             }
-            
-//             i++;
-//         }
-//         int counter = 0;
-//         while(!st.isEmpty()){
-//             counter++;
-//             st.pop();
-//         }
-//         return counter;
-        
-        int ans = 0, bal = 0;
-        for (int i = 0; i < s.length(); ++i) {
-            bal += s.charAt(i) == '(' ? 1 : -1;
-            // It is guaranteed bal >= -1
-            if (bal == -1) {
-                ans++;
-                bal++;
-            }
+        Stack<String> st = new Stack<>();
+        int i = 0;
+        int counter = 0;
+        while(i < s.length()){
+            if(s.substring(i, i+1).equals("(")){
+                st.push(")");
+                counter++;
+            }else{
+                if(!st.isEmpty() && s.substring(i, i+1).equals(st.peek())){
+                    st.pop();
+                    counter--;
+                }else{
+                    st.push("(");
+                    counter++;
+                }
+            }            
+            i++;
         }
+        return counter;
+        
+//         int ans = 0, bal = 0;
+//         for (int i = 0; i < s.length(); ++i) {
+//             bal += s.charAt(i) == '(' ? 1 : -1;
+//             // It is guaranteed bal >= -1
+//             if (bal == -1) {
+//                 ans++;
+//                 bal++;
+//             }
+//         }
 
-        return ans + bal;
+//         return ans + bal;
     }
 }
