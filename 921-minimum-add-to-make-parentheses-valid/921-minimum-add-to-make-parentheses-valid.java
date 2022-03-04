44170d288c9b53fd393d5e0a9bf48918a292250d
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-03-05 03:39:52 +0530

    Time: 1 ms (83.47%), Space: 41.9 MB (36.66%) - LeetHub

--- 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.java
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.java
@@ -1,25 +1,37 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        Stack<String> st = new Stack<>();
-        int i = 0;
-        while(i < s.length()){
-            if(s.substring(i, i+1).equals("(")){
-                st.push(")");
-            }else{
-                if(!st.isEmpty() && s.substring(i, i+1).equals(st.peek())){
-                    st.pop();
-                }else{
-                    st.push("(");
-                }
-            }
+//         Stack<String> st = new Stack<>();
+//         int i = 0;
+//         while(i < s.length()){
+//             if(s.substring(i, i+1).equals("(")){
+//                 st.push(")");
+//             }else{
+//                 if(!st.isEmpty() && s.substring(i, i+1).equals(st.peek())){
+//                     st.pop();
+//                 }else{
+//                     st.push("(");
+//                 }
+//             }
             
-            i++;
+//             i++;
+//         }
+//         int counter = 0;
+//         while(!st.isEmpty()){
+//             counter++;
+//             st.pop();
+//         }
+//         return counter;
+        
+        int ans = 0, bal = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            bal += s.charAt(i) == '(' ? 1 : -1;
+            // It is guaranteed bal >= -1
+            if (bal == -1) {
+                ans++;
+                bal++;
+            }
         }
-        int counter = 0;
-        while(!st.isEmpty()){
-            counter++;
-            st.pop();
-        }
-        return counter;
+
+        return ans + bal;
     }
 }
