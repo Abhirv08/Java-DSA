9e9c143742d3e1d75a3f18eae0d0846e91a367ce
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-03-20 13:51:43 +0530

    Time: 566 ms (10.31%), Space: 117.6 MB (5.12%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -1,26 +1,40 @@
-public class Solution {
+class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length())
-            return false;
-        int[] s1map = new int[26];
-        for (int i = 0; i < s1.length(); i++)
-            s1map[s1.charAt(i) - 'a']++;
-        for (int i = 0; i <= s2.length() - s1.length(); i++) {
-            int[] s2map = new int[26];
-            for (int j = 0; j < s1.length(); j++) {
-                s2map[s2.charAt(i + j) - 'a']++;
+        if(s1.length() > s2.length()) return false;
+        if(s1.equals(s2)) return true;
+
+        int start = 0;
+        int end = 0;
+        while(end < s2.length()+1){
+            if(s2.substring(start, end).length() < s1.length()){
+                end++;
+            }else{
+                int[] s1map = new int[26];
+                for(int i = 0; i < s1.length(); i++){
+                    s1map[s1.charAt(i) - 'a']++;
+                }
+                if(isPermutation(s1map, s2.substring(start, end))){
+                    return true;
+                }
+                start++; end++;
             }
-            if (matches(s1map, s2map))
-                return true;
         }
         return false;
     }
-    
-    public boolean matches(int[] s1map, int[] s2map) {
-        for (int i = 0; i < 26; i++) {
-            if (s1map[i] != s2map[i])
+
+    static boolean isPermutation(int[] arr, String s){
+        for(int i = 0; i < s.length(); i++){
+            if(arr[s.charAt(i) - 'a'] > 0){
+                arr[s.charAt(i) - 'a']--;
+            }
+        }
+
+        for(int i = 0; i < 26; i++){
+            if(arr[i] > 0){
                 return false;
+            }
         }
         return true;
     }
+    
 }
