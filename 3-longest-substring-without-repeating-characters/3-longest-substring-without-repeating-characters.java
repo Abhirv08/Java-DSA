e356dd1442a4e059a2f28deb42ca5572924096e5
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-05-20 16:04:44 +0530

    Time: 27 ms (21.44%), Space: 41.7 MB (99.86%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -4,13 +4,11 @@ class Solution {
         if(s.length() != 0 && s.trim().length() == 0) return 1;
         if(s.length() == 1 && s.trim().length() != 0) return 1;
         ArrayList<Character> containingItem = new ArrayList<>();
-        int k = 0;
         int l = 0;
         containingItem.add(s.charAt(l));
         int r = 1;
         int max = 1;
         int counter = 1;
-        boolean check = false;
         while(r < s.length()){
             if(!containingItem.contains(s.charAt(r))){
                 containingItem.add(s.charAt(r));
@@ -19,7 +17,6 @@ class Solution {
             }else{
                 containingItem.remove(0);
                 l++;
-                check = true;
             }
             max = Math.max(max, r-l);
         }
