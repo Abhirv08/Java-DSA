5d55f6f8d999ccde08ea74d275e42365807005cd
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-05-28 23:15:09 +0530

    Time: 26 ms (15.07%), Space: 46.3 MB (20.91%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,18 +1,28 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] countfors = new char[26];
+        HashMap<Character, Integer> memo = new HashMap<>();
         for(int i = 0; i < s.length(); i++){
-            countfors[s.charAt(i)-'a']++;
-        }
-        char[] countfort = new char[26];
-        for(int i = 0; i < t.length(); i++){
-            countfort[t.charAt(i)-'a']++;
+            char ch = s.charAt(i);
+            if(memo.containsKey(ch)){
+               memo.put(ch, memo.get(ch) + 1); 
+            }else{
+                memo.put(ch, 1);
+            }
         }
         
-        for(int i = 0; i < 26; i++){
-            if(countfort[i] < countfors[i] || countfort[i] > countfors[i]){
+        for(int i = 0; i < t.length(); i++){
+            char ch = t.charAt(i);
+            if(memo.containsKey(ch)){
+                memo.put(ch, memo.get(ch) - 1); 
+                if(memo.get(ch) == 0){
+                    memo.remove(ch);
+                }
+            }else{
                 return false;
             }
+        }
+        if(memo.size() != 0){
+            return false;
         }
         return true;
     }
