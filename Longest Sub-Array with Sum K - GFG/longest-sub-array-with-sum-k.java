bf3cf1327d39bac096a9cf4deb0839c45a7fc087
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-05-28 15:36:26 +0530

    Added solution - LeetHub

---
+++ Longest Sub-Array with Sum K - GFG/longest-sub-array-with-sum-k.java
@@ -0,0 +1,75 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    String line = br.readLine();
+		    String[] element = line.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    int K = Integer.parseInt(element[1]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    int res = obj.lenOfLongSubarr(arr, sizeOfArray, K);
+		    
+		    System.out.println(res);
+		}
+	}
+}
+
+
+            
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    
+   
+    // Function for finding maximum and value pair
+    public static int lenOfLongSubarr (int A[], int N, int k) {
+        //Complete the function
+        int psum = 0;
+        int longest = 0;
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        memo.put(0, -1);
+        for(int i = 0; i < N; i++){
+            psum += A[i];
+            if(memo.containsKey(psum-k)){
+                longest = Math.max(longest, i - memo.get(psum-k) );
+            }
+            if(!memo.containsKey(psum)){
+                memo.put(psum, i);
+            }
+        }
+        return longest;
+    }
+    
+    
+}
+
+
