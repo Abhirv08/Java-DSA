71ebdfa90220b9f956e9b575fd04899a549b45f0
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-06-07 09:42:33 +0530

    Time: 1 ms (34.98%), Space: 42.5 MB (65.17%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -7,23 +7,10 @@ class Solution {
             return;
         }
         if(n==0) return;
-        int i = 0, j = 0;
-        while(i < m && j < n){
-            if(nums1[i] <= nums2[j]){
-                i++;
-            }else{
-                int temp = nums1[i];
-                nums1[i] = nums2[j];
-                nums2[j] = temp;
-                i++;    
-                Arrays.sort(nums2);
-            }
-        }
-        while(i < m+n){
-            nums1[i] = nums2[j];
-            j++;
-            i++;
-        }
-            
+       
+        for(int i = 0; i < n; i++){
+            nums1[m+i] = nums2[i];
+        } 
+        Arrays.sort(nums1);
     }
 }
