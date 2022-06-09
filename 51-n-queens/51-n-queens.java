7e9d9f172cdc23f8643deb7e3e72d8dae2816c74
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-06-09 12:20:54 +0530

    Time: 22 ms (9.85%), Space: 46.5 MB (24.69%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -1,51 +1,65 @@
 class Solution {
-    private List<List<String>> res;
-    private int N;
-
     public List<List<String>> solveNQueens(int n) {
-        res = new ArrayList<>();
-        N = n;
-        char[][] emptyBoard = new char[N][N];
-        for (char[] row: emptyBoard) Arrays.fill(row, '.');
-        
-        backtrack(emptyBoard, 0, 0, 0, 0);
-        return res;
+        List<List<String>> ans = new ArrayList<>();
+        String[][] grid = new String[n][n];
+        for(int i =0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                grid[i][j] = ".";
+            }
+        }
+        nQueens(grid, 0, n, ans);
+        return ans;
     }
     
-    private void backtrack(char[][] board, int row, int cols, int diags, int antiDiags) {
-        // if we've successfuly placed a Queen at all rows, we have a valid board state
-        if (row == N) {
-            res.add(toBoard(board));
-            return;
+    private void nQueens(String[][] grid, int currRow, int n, List<List<String>> ans){
+        if(currRow == n){
+            List<String> temp = new ArrayList<>();
+            for(int i = 0; i < n; i++){
+                String s = "";
+                for(int j = 0; j < n; j++){
+                    s += grid[i][j];
+                }
+                temp.add(s);
+            }
+            ans.add(temp);
+            return ;
         }
         
-        for (int col=0; col<N; col++) {
-            int currDiag = row-col+N;
-            int currAntiDiag = row+col;
-            
-            // check if the current Queen placement is valid
-            if ((cols & (1 << col)) != 0 || (diags & (1 << currDiag)) != 0 || (antiDiags & (1 << currAntiDiag)) != 0) continue;
-            
-            // if so, add changes
-            board[row][col] = 'Q';
-            cols |= (1 << col);
-            diags |= (1 << currDiag);
-            antiDiags |= (1 << currAntiDiag);
-            
-            // continue to the next row
-            backtrack(board, row + 1, cols, diags, antiDiags);
-            
-            // undo changes and continue
-            board[row][col] = '.';
-            cols ^= (1 << col);
-            diags ^= (1 << currDiag);
-            antiDiags ^= (1 << currAntiDiag);
+    for(int col = 0; col < n; col++){
+        if(isValid(grid, currRow, col, n)){
+            grid[currRow][col] = "Q";
+        
+            nQueens(grid, currRow+1, n, ans);           
+        
+            grid[currRow][col] = ".";
         }
-    }
-    
-    private List<String> toBoard(char[][] board) {
-        List<String> newBoard = new ArrayList<>();
-        for (char[] row: board) newBoard.add(new String(row));
-        return newBoard;
-    }
+    }    
+    return ;
 }
+
+private boolean isValid(String[][] grid, int currRow, int currCol, int n){
+    // For Row and Column
+    for(int i = 0; i < n; i++){
+        if(grid[currRow][i].equals("Q")) return false;
+        
+        if(grid[i][currCol].equals("Q")) return false;
+    }
+    
+    // for left side Diagonal
+    int row = currRow;
+    int col = currCol;
+    while(row >= 0 && col >= 0){
+        if(grid[row][col].equals("Q")) return false;
+        row--; col--;
+    }
+    // for Right Diagonal
+    row = currRow;
+    col = currCol;
+    while(row >= 0 && col < n){
+        if(grid[row][col].equals("Q")) return false;
+        row--; col++;
+    }
+    return true;
+}
+}
+    
