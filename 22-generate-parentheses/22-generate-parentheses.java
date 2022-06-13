01a37871dbf0a60cbceed141206489475ba8d336
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-06-13 15:17:27 +0530

    Time: 3 ms (39.16%), Space: 43.4 MB (62.06%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -6,23 +6,19 @@ class Solution {
     }
     
     private void generateParenthesis(int n, int op, int cp, String s, List<String> ans){
-        if(op > n){
-            return;
-        }
+        
         
         if(op == n && cp == n){
             ans.add(s);
             return;
         }
         
-        if(op > cp){
-            generateParenthesis(n, op+1, cp, s + "(", ans);
-            generateParenthesis(n, op, cp+1, s + ")", ans);
+        if(op < n){
+            generateParenthesis(n, op+1, cp, s+"(", ans);
         }
         
-        if(op == cp){
-            generateParenthesis(n, op+1, cp, s + "(", ans);
-        }
+        if(op > cp)
+            generateParenthesis(n, op, cp+1, s + ")", ans);
         
         return;
     }
