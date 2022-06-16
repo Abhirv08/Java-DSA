3771c6f13c27e8a6e943715507b6bac5e5d8eb76
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-06-16 13:26:08 +0530

    Time: 468 ms (20.46%), Space: 84.6 MB (19.27%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,41 +1,29 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int n = s.length();
-      if(n == 1) return s;
-      if(n == 2){
-         if (s.charAt(0) == s.charAt(1)) return s;
-         else return s.substring(0,1);
-      }
-      boolean[][] dp = new boolean[n][n];
-      int[] max = new int[2];
-      int i = 0; int j = 0;
-      boolean flag = true;
-      while(flag){
-         if(i == j) {
-            dp[i][j] = true;
-         }else{
-            if(s.charAt(i) == s.charAt(j)){
-               if(i + 1 == j || dp[i+1][j-1]){
-                  dp[i][j] = true;
-               }
+      int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int[] max = new int[2];
+        for(int g = 0; g < n; g++){
+            for(int i = 0, j = g; j < n; i++, j++){
+                if(g == 0) dp[i][j] = true;
+                else if(g == 1 && s.charAt(i) == s.charAt(j)){
+                    dp[i][j] = true;
+                }
+                else{
+                    if(s.charAt(i) == s.charAt(j) && dp[i+1][j-1]){
+                        dp[i][j] = true;
+                    }
+                }
+                
+                if(dp[i][j] && max[1] - max[0] < j - i){
+                    max[0] = i;
+                    max[1] = j;
+                }
             }
-         }
-         if(dp[i][j] && max[1] - max[0] <= j - i){
-            max[0] = i;
-            max[1] = j;
-         }
-         i++;
-         j++;
-         if(j == n){
-            if(i == 1 && j == n) break;
-            j = n + 1 - i;
-            i = 0;
-
-         }
-      }
-
-
-      return s.substring(max[0], max[1] + 1);
+        }
+        
+        
+        return s.substring(max[0], max[1] + 1);
         
         
     }
