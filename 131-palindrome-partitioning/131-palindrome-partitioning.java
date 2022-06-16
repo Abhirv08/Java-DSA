8732672192cd14b8e6c36605402e5202743820c7
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-06-16 13:59:49 +0530

    Time: 15 ms (69.81%), Space: 192.4 MB (49.32%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -1,26 +1,21 @@
 class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> ans = new ArrayList<>();
-        generateAllCombs(s, 0, s.length(), new ArrayList<>(), ans);
-        
-        return  ans;
+        subStringGenerator(s, 0, new ArrayList<>(), ans);
+        return ans;
     }
     
-    private void generateAllCombs(String s, int start, int end, List<String> list, List<List<String>> ans){
-        if(start >= end){
-            List<String> temp = new ArrayList<>();
-            for(int i = 0; i < list.size(); i++){
-                temp.add(list.get(i));
-            }
-            ans.add(temp);
+    private void subStringGenerator(String s, int index, List<String> list, List<List<String>> ans){
+        if(index == s.length()){            
+            ans.add(new ArrayList<>(list));
             return;
-        } 
+        }
         
-        for(int i = start; i < end; i++){
-            String leftpart = s.substring(start, i+1);
+        for(int i = index; i < s.length(); i++){
+            String leftpart = s.substring(index, i+1);
             if(isPalindrome(leftpart)){
                 list.add(leftpart);
-                generateAllCombs(s, i+1, end, list, ans);
+                subStringGenerator(s, i+1, list, ans);
                 list.remove(list.size() - 1);
             }
         }
@@ -29,12 +24,12 @@ class Solution {
     }
     
     private boolean isPalindrome(String s){
-        int i = 0; 
-        int j = s.length() - 1;
-        while(i < j){
-            if(s.charAt(i) != s.charAt(j)) return false;
-            
-            i++; 
+        int i = 0, j = s.length()-1;
+        while( i <= j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
             j--;
         }
         return true;
