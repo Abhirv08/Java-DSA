7bf00ff3e9049c0426afe5e86c32187963c78396
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-06-16 15:45:25 +0530

    Time: 678 ms (5.06%), Space: 118.6 MB (5.47%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.java
@@ -1,16 +1,25 @@
 public class Solution {
     public int minDistance(String s1, String s2) {
-        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
-        for (int i = 0; i <= s1.length(); i++) {
-            for (int j = 0; j <= s2.length(); j++) {
-                if (i == 0 || j == 0)
-                    continue;
-                if (s1.charAt(i - 1) == s2.charAt(j - 1))
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                else
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
-        return s1.length() + s2.length() - 2 * dp[s1.length()][s2.length()];
+        return minDistance(s1, 0, s2, 0, new HashMap<>());        
     }
+    
+    private int minDistance(String s1, int i1, String s2, int i2, HashMap<String, Integer> memo){
+        if(i1 == s1.length() && i2 == s2.length()) return 0;
+        if(i1 == s1.length() || i2 == s2.length()) return Math.max(s1.length() - i1, s2.length() - i2);
+        String s = s1.substring(i1) + ":" + s2.substring(i2);
+        if(memo.containsKey(s)){
+            return memo.get(s);
+        }
+        
+        if(s1.charAt(i1) == s2.charAt(i2)){
+            int d = minDistance(s1, i1+1, s2, i2+1, memo);
+            memo.put(s, d);
+            return d;
+        }
+            int removedFroms1 = minDistance(s1, i1+1, s2, i2, memo);
+            int removedFroms2 = minDistance(s1, i1, s2, i2+1, memo);
+            
+            memo.put(s, Math.min(removedFroms1, removedFroms2) + 1);
+        return Math.min(removedFroms1, removedFroms2) + 1;
+    } 
 }
