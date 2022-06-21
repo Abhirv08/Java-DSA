9c7e7a75cd59c9dcdf05722390172cadaf501593
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-06-21 14:24:12 +0530

    Time: 8 ms (16.02%), Space: 54.4 MB (24.10%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.java
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -6,11 +6,27 @@ class Solution {
         boolean[] visited = new boolean[n];
         for(int currentCity = 0; currentCity < n; currentCity++){
             if(!visited[currentCity] ){
-                dfs(isConnected, visited, currentCity);
+                // dfs(isConnected, visited, currentCity);
+                bfs(isConnected, visited, currentCity);
                 ans++;
             }
         }
         return ans;
+    }
+    
+    private void bfs(int[][] iC, boolean[] visited, int currentCity){   
+        
+        Stack<Integer> st = new Stack<>();
+        st.add(currentCity);
+        while(!st.isEmpty()){
+            int city = st.pop();
+            visited[city] = true;
+            for(int neighbour = 0; neighbour < iC[city].length; neighbour++){
+                if(iC[city][neighbour] > 0 && !visited[neighbour]){
+                    st.add(neighbour);
+                }
+            }
+        }
     }
     
     private void dfs(int[][] iC, boolean[] visited, int currentCity){        
