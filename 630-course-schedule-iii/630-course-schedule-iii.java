11bfde6bcfe9f40b47b18e4c38a67eb51c7a07f6
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-06-23 23:42:48 +0530

    Time: 74 ms (25.00%), Space: 75.3 MB (10.00%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.java
+++ 630-course-schedule-iii/630-course-schedule-iii.java
@@ -1,26 +1,25 @@
 class Solution {
     public int scheduleCourse(int[][] courses){
+        PriorityQueue<Integer> pq = new PriorityQueue<>( (a,b) -> b-a );
         int time = 0;
-        int coursesTaken = 0;
         
-        Arrays.sort(courses, (a, b) -> a[1] - b[1]);
-        for(int i = 0; i < courses.length; i++){
-            if (time + courses[i][0] <= courses[i][1]) {
-                time += courses[i][0];
-                coursesTaken++;
-            } else {
-                int max_i = i;
-                for (int j = 0; j < i; j++) {
-                    if (courses[j][0] > courses[max_i][0])
-                        max_i = j;
+        Arrays.sort(courses, (a, b) -> a[1] == b[1] ? a[0] - b[0] : a[1] - b[1] );
+        for(int[] course : courses){
+            
+            if(course[0] <= course[1]){
+                if(time + course[0] <= course[1]){
+                    pq.add(course[0]);
+                    time += course[0];
+                }else{
+                    if(pq.peek() > course[0]){
+                        time -= pq.poll();
+                        time += course[0];
+                        pq.add(course[0]);
+                    }
                 }
-                if (courses[max_i][0] > courses[i][0]) {
-                    time += courses[i][0] - courses[max_i][0];
-                }
-                courses[max_i][0] = -1;
-            }        
+            }
         }
         
-        return coursesTaken;
+        return pq.size();
     }    
 }
