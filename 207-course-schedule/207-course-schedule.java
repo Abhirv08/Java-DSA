8a7934ddd988401cd8ac2cd0b97add0f87e6ccab
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-06-24 11:51:34 +0530

    Time: 9 ms (52.50%), Space: 47.8 MB (60.32%) - LeetHub

--- 207-course-schedule/207-course-schedule.java
+++ 207-course-schedule/207-course-schedule.java
@@ -1,10 +1,13 @@
 class Solution {
-    public boolean canFinish(int v, int[][] adj) {
-       int[] inDegree = new int[v];
+    public boolean canFinish(int v, int[][] prerequisites ) {
+                ArrayList<ArrayList<Integer>> adj = constructGraph(v, prerequisites);
+        int[] inDegree = new int[v];
         
         // find indegree of each courses
-        for(int i = 0; i < adj.length; i++){
-            inDegree[adj[i][1]]++;
+        for(int i = 0; i < v; i++){
+            for(int course : adj.get(i)){
+                inDegree[course]++;
+            }
         }
         
         Queue<Integer> q = new LinkedList<>();
@@ -25,18 +28,33 @@ class Solution {
                 continue;
             
             visited[currentVertex] = true;
+            
             visitedVertex++;
             
-            for(int i = 0; i < adj.length; i++){
-                if(adj[i][0] == currentVertex){
-                    inDegree[adj[i][1]]--;
-                    if(inDegree[adj[i][1]] == 0){
-                        q.add(adj[i][1]);
+            for(int neighbour : adj.get(currentVertex)){
+                inDegree[neighbour]--;
+                    if(inDegree[neighbour] == 0){
+                        q.add(neighbour);
                     }
-                }
             }
         }     
         
         return visitedVertex == v ? true : false;
+    }
+    
+     private ArrayList<ArrayList<Integer>> constructGraph(int v, int[][] prerequisites ){
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        
+        for(int i = 0; i < v; i++){
+            graph.add(new ArrayList<>());
+        }
+        
+        for(int[] edges : prerequisites){
+            int destination = edges[0];
+            int source = edges[1];
+            
+            graph.get(source).add(destination);
+        }
+        return graph;
     }
 }
