7223c5a24ff7c7fc2f0da52b1cd39259631d37e5
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-06-30 16:59:36 +0530

    Time: 5 ms (66.60%), Space: 46.3 MB (11.76%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -3,25 +3,18 @@ class Solution {
         Arrays.sort(nums);
         int n = nums.length;
         int ans = 0;
-        if( (n & 1) == 1){
-            int median = n/2;
-            ans = findSteps(nums, median);            
-        }else{            
-            ans = Math.min( findSteps(nums, n/2), findSteps(nums, n/2-1) );
-        }
         
+        int i = 0;
+        int j = n-1;
+        
+        while( i < j){
+            ans += nums[j] - nums[i];
+            i++;
+            j--;
+        }
         
         
         return ans;
     }
-    
-    private int findSteps(int[] nums, int median){
-        int steps = 0;
-        
-        for(int i = 0; i < nums.length; i++){
-            steps += Math.abs(nums[i] - nums[median]);
-        }
-        
-        return steps;
-    }
+
 }
