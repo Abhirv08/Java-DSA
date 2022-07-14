4209285fccb85d8d5140e94b9457646f1d1166fc
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-07-14 22:12:33 +0530

    Time: 69 ms (15.89%), Space: 59.2 MB (9.32%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,6 +1,37 @@
 class Solution {
-    public int coinChange(int[] coins, int amount) {                
-        return countCoins(coins, amount, coins.length);
+    public int coinChange(int[] coins, int amount) {    
+        int[][] memo = new int[coins.length + 1][amount+1];
+        
+        for(int[] arr : memo){
+            Arrays.fill(arr, -1);
+        }
+        int ans = countCoins(coins, amount, coins.length, memo);
+        return ans == Integer.MAX_VALUE - 1 ? -1 : ans ;
+    }
+    
+    private int countCoins(int[] coins, int amount, int n, int[][] memo){
+        if(amount == 0){
+            return 0;
+        }
+        
+        if(n == 0){
+            return Integer.MAX_VALUE - 1;
+        }
+        
+        if(memo[n][amount] != -1){
+            return memo[n][amount];
+        }
+        
+        if(coins[n-1] > amount){
+            return countCoins(coins, amount, n-1, memo);
+        }
+        
+        int withConsidering = 1 + countCoins(coins, amount - coins[n-1], n, memo);
+        int withoutConsidering = countCoins(coins, amount, n - 1, memo);
+        
+        memo[n][amount] = Math.min(withoutConsidering, withConsidering);
+        
+        return Math.min(withoutConsidering, withConsidering);
     }
     
     private int countCoins(int[] coins, int amount, int n){
@@ -9,7 +40,6 @@ class Solution {
         for(int j = 0; j < amount+1; j++){
             memo[0][j] = Integer.MAX_VALUE - 1;
         }
-        
         
         for(int i = 1; i < n+1; i++){
             for(int j = 1; j < amount+1; j++){
