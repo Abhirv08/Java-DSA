529705cc5bf6fb57e9321ee7e352cf219cacfcbe
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-08-05 19:22:05 +0530

    Time: 12 ms (94.79%), Space: 46.3 MB (65.72%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,43 +1,22 @@
 class Solution {
-    public int longestCommonSubsequence(String t1, String t2) {
-        int t1_size = t1.length();
-        int t2_size = t2.length();
+    public int longestCommonSubsequence(String s1, String s2) {
+        int n1 = s1.length();
+        int n2 = s2.length();
         
-        int[][] memo = new int[t1_size + 1][t2_size + 1];
+        int[][] dp = new int[n1 + 1][n2 + 1];
         
-        for(int i = 1; i < t1_size + 1; i++){
-            for(int j = 1; j < t2_size + 1; j++){
-                if(t2.charAt(j - 1) == t1.charAt(i - 1)){
-                    memo[i][j] = 1 + memo[i-1][j-1];
+        for(int i1 = 1; i1 < n1 + 1; i1++){
+            for(int i2 = 1; i2 < n2 + 1; i2++){
+                if(s1.charAt(i1 - 1) == s2.charAt(i2 - 1)){
+                    dp[i1][i2] = 1 + dp[i1-1][i2-1];
                 }else{
-                    memo[i][j] = Math.max(memo[i - 1][j], memo[i][j-1]);
+                    dp[i1][i2] = Math.max(dp[i1-1][i2], dp[i1][i2-1]);
                 }
             }
         }
         
-        return memo[t1_size][t2_size];       
-        
-        
+        return dp[n1][n2];
     }
     
-    private int lcs(String t1, String t2, int i1, int i2, int[][] memo){
-        if(i1 == 0 || i2 == 0){
-            return 0;
-        }
-        
-        if(memo[i1][i2] != -1){
-            return memo[i1][i2];
-        }
-        
-        if(t1.charAt(i1 - 1) == t2.charAt(i2 - 1)){
-            return 1 + lcs(t1, t2, i1 - 1, i2 - 1, memo);
-        }
-        
-        int p1 = lcs(t1, t2, i1 - 1, i2, memo);
-        int p2 = lcs(t1, t2, i1, i2 - 1, memo);
-        
-        memo[i1][i2] = Math.max(p1, p2);
-        
-        return Math.max(p1, p2);
-    }
+    
 }
