1f3f9bf874d2090365a6865d68e162faaed37854
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-08-06 16:09:26 +0530

    Time: 2 ms (98.85%), Space: 42.6 MB (85.97%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,36 +1,23 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < nums2.length; i++){
+            map.put(nums2[i] , i);
+        }
+        
         int[] ans = new int[nums1.length];
-        int pos = 0;
-        for(int i = 0; i < nums1.length; i++){
-            int num = nums1[i];
-            int index = matchingIndex(num, nums2);
-            if(index==nums2.length-1){
-                ans[pos] = -1;
-            }else{
-                for(int j = index + 1; j < nums2.length; j++){
-                    if(nums2[j] > num){
-                        ans[pos] = nums2[j];
-                        break;
-                    }
-                }
-                if(ans[pos]==0){
-                    ans[pos] = -1;
+        for(int i = 0 ; i < nums1.length; i++){
+            int nextGreater = -1;
+            for(int j = map.get(nums1[i]) + 1; j < nums2.length; j++){
+                if(nums2[j] > nums1[i]){
+                    nextGreater = nums2[j];
+                    break;
                 }
             }
-            pos++;
+            ans[i] = nextGreater;
         }
         
         return ans;
     }
-    
-    static int matchingIndex(int num, int[] nums2){
-        for(int i = 0; i < nums2.length; i++){
-            if(num==nums2[i]){
-                return i;
-            }
-        }
-        return -1;
-    }
-    
 }
