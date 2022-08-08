2494a92394ace4b3bfea362fd3d37a2be736792c
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-08-08 16:16:08 +0530

    Time: 139 ms (14.76%), Space: 44.2 MB (62.72%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -2,27 +2,31 @@ class Solution {
     public int lengthOfLIS(int[] nums) {
         int ans = 0;
         int n = nums.length;
-        int[][] dp = new int[n + 1][n + 1];
+        // int[][] dp = new int[n + 1][n + 1];
         
         
         // for(int[] t : dp){
         //     Arrays.fill(t, -1);
         // }
         
+        int[] next = new int[n + 1];
+        int[] curr = new int[n + 1];
+        
         int lastNumTaken = 0;
         for(int index = n-1; index >= 0; index--){            
             for(int prev_ind = index - 1; prev_ind >= -1; prev_ind--){
-                int len = dp[index + 1][prev_ind + 1];
+                int len = next[prev_ind + 1];
         
                 if(prev_ind == -1 || nums[index] > nums[prev_ind]){
-                    len =  Math.max(len, 1 + dp[index + 1][index + 1]);
+                    len =  Math.max(len, 1 + next[index + 1]);
                 }   
         
-                dp[index][prev_ind + 1] = len;
+                curr[prev_ind + 1] = len;
             }
+            next = curr;
         }
         
-        return dp[0][-1+1];
+        return next[-1+1];
     }
     
     private int lengthOfLIS(int[] nums, int index, int prev_ind, int[][] dp){
