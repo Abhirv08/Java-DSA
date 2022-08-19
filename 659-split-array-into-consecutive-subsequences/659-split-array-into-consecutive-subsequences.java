8a41cec5c7158fbbfd90dcff629292ba432d1625
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-08-19 17:45:12 +0530

    Time: 35 ms (58.64%), Space: 69.1 MB (56.14%) - LeetHub

---
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean isPossible(int[] nums) {
+        int n = nums.length;
+        if(n < 3){
+            return false;
+        }
+        
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        
+        for(int num : nums){
+            memo.put(num, memo.getOrDefault(num, 0) + 1);
+        }
+        
+        HashMap<Integer, Integer> end = new HashMap<>();
+        
+        for(int num : nums){
+            if(memo.get(num) == 0) continue;           
+            
+            
+            if(end.get(num - 1) != null && end.get(num - 1) > 0){ 
+                end.put(num - 1, end.get(num - 1) - 1);
+                end.put(num, end.getOrDefault(num, 0) + 1);
+                memo.put(num, memo.get(num) - 1);
+            }else{
+                if(memo.getOrDefault(num + 1, 0) > 0 && memo.getOrDefault(num + 2, 0) > 0){
+                    memo.put(num, memo.get(num) - 1);
+                    memo.put(num + 1, memo.get(num + 1) - 1);
+                    memo.put(num + 2, memo.get(num + 2) - 1);
+                    end.put(num + 2, end.getOrDefault(num + 2, 0) + 1);                    
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
