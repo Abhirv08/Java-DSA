5b68e94143d7c91b3b3bc46dade7c1fef5d4f7e2
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-08-19 17:46:32 +0530

    Time: 63 ms (21.36%), Space: 68 MB (64.09%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.java
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.java
@@ -17,19 +17,16 @@ class Solution {
             if(memo.get(num) == 0) continue;           
             
             
+            memo.put(num, memo.get(num) - 1);
             if(end.get(num - 1) != null && end.get(num - 1) > 0){ 
                 end.put(num - 1, end.get(num - 1) - 1);
                 end.put(num, end.getOrDefault(num, 0) + 1);
-                memo.put(num, memo.get(num) - 1);
+            }else if(memo.getOrDefault(num + 1, 0) > 0 && memo.getOrDefault(num + 2, 0) > 0){
+                memo.put(num + 1, memo.get(num + 1) - 1);
+                memo.put(num + 2, memo.get(num + 2) - 1);
+                end.put(num + 2, end.getOrDefault(num + 2, 0) + 1);                    
             }else{
-                if(memo.getOrDefault(num + 1, 0) > 0 && memo.getOrDefault(num + 2, 0) > 0){
-                    memo.put(num, memo.get(num) - 1);
-                    memo.put(num + 1, memo.get(num + 1) - 1);
-                    memo.put(num + 2, memo.get(num + 2) - 1);
-                    end.put(num + 2, end.getOrDefault(num + 2, 0) + 1);                    
-                }else{
-                    return false;
-                }
+                return false;
             }
         }
         return true;
