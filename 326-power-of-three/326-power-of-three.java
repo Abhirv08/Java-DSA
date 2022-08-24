0a8acf19aa60d3acf9c8d09936c996546b50c6e6
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-08-24 17:59:23 +0530

    Time: 52 ms (5.17%), Space: 54.4 MB (6.36%) - LeetHub

--- 326-power-of-three/326-power-of-three.java
+++ 326-power-of-three/326-power-of-three.java
@@ -1,17 +1,7 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
+        return Integer.toString(n, 3).matches("^10*$");
         
-        int i = 0;
-        long num = (long)Math.pow(3, i);
-        while(num <= n){
-            if(num == n){
-                return true;
-            }
-            
-            num = (long)Math.pow(3, i);
-            i++;
-        }
         
-        return false;
     }
 }
