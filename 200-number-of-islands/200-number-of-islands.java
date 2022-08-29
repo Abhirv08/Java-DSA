b68bf296e5008b2b05dcb150a23d54a38b7708da
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-08-29 18:27:23 +0530

    Time: 6 ms (39.74%), Space: 57.3 MB (55.32%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,32 +1,32 @@
 class Solution {
     public int numIslands(char[][] grid) {
         int ans = 0;
-        int m = grid.length;
-        int n = grid[0].length;
         
-        for(int currentRow = 0; currentRow < m; currentRow++){
-            for(int currentCol = 0; currentCol < n; currentCol++){
-                if(grid[currentRow][currentCol] == '1'){
-                    sinkIsland(grid, currentRow, currentCol, m, n);
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == '1'){
+                    sinkIsland(grid, i, j);
                     ans++;
                 }
             }
         }
+        
         return ans;
     }
     
-    private void sinkIsland(char[][] grid, int currentRow, int currentCol, int m, int n){
-        if(currentRow < 0 || currentRow >= m || currentCol < 0 || currentCol >= n || grid[currentRow][currentCol] == '0'){
-            return;
+    private void sinkIsland(char[][] grid, int row, int col){
+        if( row < 0 || row == grid.length || col < 0 || col == grid[0].length || grid[row][col] == '0'){
+            return ;
         }
         
-        grid[currentRow][currentCol] = '0';
+        grid[row][col] = '0';
         
-        sinkIsland(grid, currentRow - 1, currentCol, m, n);
-        sinkIsland(grid, currentRow + 1, currentCol, m, n);
-        sinkIsland(grid, currentRow, currentCol - 1, m, n);
-        sinkIsland(grid, currentRow, currentCol + 1, m, n);
+        sinkIsland(grid, row - 1, col);
+        sinkIsland(grid, row, col + 1);
+        sinkIsland(grid, row + 1, col);
+        sinkIsland(grid, row, col - 1); 
         
-        return;
+        
     }
+    
 }
