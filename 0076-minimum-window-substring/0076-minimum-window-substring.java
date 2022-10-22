a77ce218a8aeebe238e9893072057880f299dcf0
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-10-22 15:37:39 +0530

    Time: 13 ms (78.36%), Space: 43.1 MB (88.05%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -3,25 +3,21 @@ class Solution {
         if(t.length() > s.length()) return "";
         
         HashMap<Character, Integer> map = new HashMap<>();
-        HashMap<Character, Integer> original = new HashMap<>();
         for(char ch: t.toCharArray()){
             map.put(ch, map.getOrDefault(ch, 0) + 1);
-            original.put(ch, original.getOrDefault(ch, 0) + 1);
         }
         
         String ans = "";
         int count = t.length();
         for(int acq = 0, rel = 0; acq < s.length();){
             char key = s.charAt(acq);
-            if(count > 0){
-                if(map.containsKey(key)){
-                    if(map.get(key) > 0){
-                        count--;
-                    }
-                    map.put(key, map.get(key) - 1);
+            if(map.containsKey(key)){
+                if(map.get(key) > 0){
+                    count--;
                 }
-                acq++;
+                map.put(key, map.get(key) - 1);
             }
+            acq++;
             
             while(count == 0){
                 key = s.charAt(rel);
