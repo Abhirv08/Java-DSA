471994382d5fd66c7708663204ad5712e110bd32
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-10-23 11:09:14 +0530

    Time: 1 ms (90.00%), Space: 41.3 MB (60.00%) - LeetHub

--- 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.java
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.java
@@ -1,28 +1,5 @@
 class Solution {
-    public boolean haveConflict(String[] event1, String[] event2) {
-        String[] e1s = event1[0].split(":");
-        String[] e1e = event1[1].split(":");
-        String[] e2s = event2[0].split(":");
-        String[] e2e = event2[1].split(":");
-
-        int s1 = giveTime(e1s);
-        int e1 = giveTime(e1e);
-        int s2 = giveTime(e2s);
-        int e2 = giveTime(e2e);
-
-        // if 2nd event starting before completion of 1st event
-        if(e2 >= s1 && e1 >= s2){
-            return true;
-        }
-
-        return false;
+    public boolean haveConflict(String[] e1, String[] e2) {
+        return e1[0].compareTo(e2[1]) <= 0 && e2[0].compareTo(e1[1]) <= 0;
     }
-
-    private int giveTime(String[] arr){
-        int time = Integer.parseInt(arr[0])*60;
-        time += Integer.parseInt(arr[1]);
-
-        return time;
-    }
-    
 }
