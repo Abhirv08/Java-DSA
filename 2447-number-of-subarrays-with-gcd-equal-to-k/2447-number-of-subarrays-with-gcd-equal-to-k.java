6ea7adac14bc51c377d06320bf56de82c6f93e95
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-10-25 11:21:06 +0530

    Time: 1191 ms (5.72%), Space: 43.1 MB (24.88%) - LeetHub

---
+++ 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int subarrayGCD(int[] nums, int k) {
+        int subarrays = 0;
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i; j < nums.length; j++){
+                if(findGCD(nums, i, j) == k){
+                    subarrays++;
+                }
+            }
+        }
+        
+        return subarrays;
+    }
+    
+    static int gcd(int a, int b)
+    {
+        if (a == 0)
+            return b;
+        return gcd(b % a, a);
+    }
+ 
+    static int findGCD(int arr[], int i, int j)
+    {
+        int result = arr[i];
+        for (; i <= j; i++){
+            result = gcd(result, arr[i]);
+ 
+            if(result == 1)
+            {
+               return 1;
+            }
+        }
+ 
+        return result;
+    }
+    
+}
