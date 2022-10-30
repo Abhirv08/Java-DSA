8c45ba42029b257d4c90794b451c7ea14d925cbe
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-10-30 18:51:11 +0530

    Time: 72 ms (40.76%), Space: 55.3 MB (33.12%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -1,51 +1,39 @@
 class Solution {
     public int shortestPath(int[][] grid, int k) {
-        int m = grid.length;
-        int n = grid[0].length;
+        int m = grid.length, n = grid[0].length;
         
-        int[][] visited = new int[m][n];
-        for(int[] r: visited){
-            Arrays.fill(r, -1);
-        }
-        
+        boolean[][][] vis = new boolean[m][n][k+1];
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0, 0, 0, k});
+        q.add(new int[]{0, 0, k});
+        int steps = 0;
+        int[][] dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
         
         while(!q.isEmpty()){
-            int[] currStat = q.remove();
-            int row = currStat[0];
-            int col = currStat[1];
-            int dist = currStat[2];
-            int remk = currStat[3];
-            
-            if(row < 0 || row == m || col < 0 || col == n) 
-                continue;
-            
-            if(visited[row][col] != -1 && visited[row][col] >= remk){
-                continue;
+            int size = q.size();
+            while(size-- > 0){
+                int[] curr = q.poll();
+                
+                if(curr[0] == m - 1 && curr[1] == n - 1) return steps;
+                
+                for(int[] d: dir){
+                    int newR = curr[0] + d[0];
+                    int newC = curr[1] + d[1];
+                    int obs = curr[2];
+                    
+                    if(newR >= 0 && newR < m && newC >= 0 && newC < n){
+                        if(grid[newR][newC] == 0 && !vis[newR][newC][obs]){
+                            q.add(new int[]{newR, newC, obs});
+                            vis[newR][newC][obs] = true;
+                        }else if(grid[newR][newC] == 1 && obs > 0 && !vis[newR][newC][obs - 1]){
+                            q.add(new int[]{newR, newC, obs - 1});
+                            vis[newR][newC][obs - 1] = true;
+                        }
+                    }
+                    
+                }               
             }
-            
-            if(row == m-1 && col == n-1){
-                return dist;
-            }
-            
-            if(grid[row][col] == 1){
-                if(remk > 0) 
-                    remk--;
-                else 
-                    continue;
-            }
-            
-            
-            
-            visited[row][col] = remk;
-            
-            q.add(new int[]{row-1, col, dist+1, remk});
-            q.add(new int[]{row, col+1, dist+1, remk});
-            q.add(new int[]{row+1, col, dist+1, remk});
-            q.add(new int[]{row, col-1, dist+1, remk});
+            steps++;
         }
-        
         return -1;
     }
 }
