f254cab602f9f95faefd14d7498be4a864067b9f
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-11-02 19:53:04 +0530

    Time: 15 ms (11.41%), Space: 43 MB (6.42%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
@@ -3,6 +3,11 @@ class Solution {
         Queue<String> q = new LinkedList<>();
         q.add(start);
         
+        Set<String> setBank = new HashSet<>();
+        for(String s: bank){
+            setBank.add(s);
+        }
+        
         Set<String> seen = new HashSet<>();
         seen.add(start);
         
@@ -16,7 +21,7 @@ class Solution {
                 for(char ch: new char[]{'A', 'C', 'G', 'T'}){
                     for(int i = 0; i < 8; i++){
                         String neighbour = node.substring(0, i) + ch + node.substring(i+1);
-                        if(!seen.contains(neighbour) && Arrays.asList(bank).contains(neighbour)){
+                        if(!seen.contains(neighbour) && setBank.contains(neighbour)){
                             q.add(neighbour);
                             seen.add(neighbour);
                         }
