e14c61c51d34871c5a96116c0b0462b7919f39d9
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-11-04 14:43:31 +0530

    Time: 49 ms (19.29%), Space: 47.4 MB (22.87%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,24 +1,44 @@
 class Solution {
     public String reverseVowels(String s) {
-        int left = 0, right = s.length() - 1;
+//         int left = 0, right = s.length() - 1;
         
-        while(left < right){
-            while(left < right && !isVowel(s.charAt(left))){
-                left++;
-            }
+//         while(left < right){
+//             while(left < right && !isVowel(s.charAt(left))){
+//                 left++;
+//             }
             
-            while(left < right && !isVowel(s.charAt(right))){
-                right--;
-            }
+//             while(left < right && !isVowel(s.charAt(right))){
+//                 right--;
+//             }
             
-            if(left < right){
-                s = s.substring(0, left) + s.charAt(right) + s.substring(left+1, right) + s.charAt(left) + s.substring(right + 1);
+//             if(left < right){
+//                 s = s.substring(0, left) + s.charAt(right) + s.substring(left+1, right) + s.charAt(left) + s.substring(right + 1);
+//             }
+//             left++;
+//             right--;
+//         }
+        
+//         return s;
+        
+        ArrayList<Character> list = new ArrayList<>();
+        
+        for(char ch: s.toCharArray()){
+            if(isVowel(ch)){
+                list.add(ch);
             }
-            left++;
-            right--;
         }
         
-        return s;
+        int idx = list.size() - 1;
+        StringBuilder ans = new StringBuilder(s);
+        
+        for(int i = 0; i < ans.length(); i++){
+            if(isVowel(ans.charAt(i))){
+                ans.replace(i, i+1, Character.toString(list.get(idx)));
+                idx--;
+            }
+        }
+        
+        return ans.toString();
     }
     
     boolean isVowel(char ch){
