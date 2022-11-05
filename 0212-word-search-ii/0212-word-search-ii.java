980941300d027fcae64fd490dfe318d73281e95e
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-11-05 17:42:26 +0530

    Time: 287 ms (57.20%), Space: 45 MB (48.19%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.java
+++ 0212-word-search-ii/0212-word-search-ii.java
@@ -13,17 +13,17 @@ class Trie{
 }
 class Solution {
     Trie root;
-    List<String> list;
+    
     public List<String> findWords(char[][] board, String[] words) {
         root = new Trie();
-        list = new ArrayList<>();
+        List<String> list = new ArrayList<>();
         for(String word: words){
             insert(word);
         }
         
         for(int row = 0; row < board.length; row++){
             for(int col = 0; col < board[0].length; col++){
-                isPresent(board, board.length, board[0].length, row, col, root);
+                isPresent(board, board.length, board[0].length, row, col, root, list);
             }
         }
         
@@ -44,7 +44,7 @@ class Solution {
         return ;
     }
     
-    private void isPresent(char[][] board, int rows, int cols, int row, int col, Trie node){
+    private void isPresent(char[][] board, int rows, int cols, int row, int col, Trie node, List<String> list){
         if(row < 0 || row == rows|| col < 0 || col == cols || board[row][col] == '.'){
             return ;
         }
@@ -62,10 +62,10 @@ class Solution {
         
         board[row][col] = '.';
         
-        isPresent(board, rows, cols, row - 1, col, node);
-        isPresent(board, rows, cols, row, col + 1, node);
-        isPresent(board, rows, cols, row + 1, col, node);
-        isPresent(board, rows, cols, row, col - 1, node);
+        isPresent(board, rows, cols, row - 1, col, node, list);
+        isPresent(board, rows, cols, row, col + 1, node, list);
+        isPresent(board, rows, cols, row + 1, col, node, list);
+        isPresent(board, rows, cols, row, col - 1, node, list);
         
         board[row][col] = ch;
         return ;
