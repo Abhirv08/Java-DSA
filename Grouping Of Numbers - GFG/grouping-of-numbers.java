7119db0d744e4edb4b928387509bb2cad1292490
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-11-05 15:12:39 +0530

    Added solution - LeetHub

---
+++ Grouping Of Numbers - GFG/grouping-of-numbers.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S[] = read.readLine().split(" ");
+            
+            int N = Integer.parseInt(S[0]);
+            int K = Integer.parseInt(S[1]);
+            
+            String S1[] = read.readLine().split(" ");
+            
+            int arr[] = new int[N];
+            
+            for(int i=0; i<N; i++)
+                arr[i] = Integer.parseInt(S1[i]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.maxGroupSize(arr,N,K));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    static int maxGroupSize(int[] arr, int N, int K) {
+        int[] freq = new int[K];
+        for(int num: arr){
+            freq[num%K]++;
+        }
+        
+        int ans = 0;
+        for(int i = 1; i <= K/2; i++){
+            if(i != K - i){
+                ans += Math.max(freq[i], freq[K - i]);
+            }else{
+                if(freq[i] > 0){
+                    ans++;
+                }
+            }
+        }
+        
+        if(freq[0] > 0){
+            ans++;
+        }
+        
+        
+        return ans;   
+    }
+};
