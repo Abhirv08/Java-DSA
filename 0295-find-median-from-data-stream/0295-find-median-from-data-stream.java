fc9fc390649b6a0b86a7aa33c9ee2a29915e9260
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-11-12 18:13:25 +0530

    Time: 204 ms (67.99%), Space: 125.4 MB (33.36%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -1,41 +1,42 @@
 class MedianFinder {
-    List<Integer> list;
+    
+    // To store lower half of data stream eg. 1, 2, 3, 6
+    PriorityQueue<Integer> lowerHalf;
+    // To store upper half of data stream eg. 8, 9, 11
+    PriorityQueue<Integer> upperHalf;
+
+    /** initialize your data structure here. */
     public MedianFinder() {
-        list = new ArrayList<>();
+        lowerHalf = new PriorityQueue<>((a,b) -> b - a); // Max heap : To fetch largest
+        // element from lower half in O(1) time
+        upperHalf = new PriorityQueue<>(); // Min heap : To fetch lowest 
+        // element from upper half in O(1) time
     }
     
     public void addNum(int num) {
-        int i = 0, j = list.size() - 1;
-        
-        while(i <= j){
-            int mid = i + (j - i)/2;
-            
-            if(list.get(mid) == num){
-                list.add(mid, num);
-                return;
-            }else if(list.get(mid) > num){
-                j = mid - 1;
-            }else{
-                i = mid + 1;
-            }
+        // Insert in lowerHalf is it's empty or if number being inserted is less than the peek of lowerHalf otherwise insert in upperHalf
+        if(lowerHalf.isEmpty() || num <= lowerHalf.peek()){
+            lowerHalf.add(num);
+        }else{
+            upperHalf.add(num);
         }
         
-        list.add(i, num);
-        return;
+        // We also need to ensure that the halves are balanced i.e. there is no more than a difference of 1 in size of both halves
+        // Let lowerHalf be the one to hold one extra element if the size of total data stream is odd otherwise be equal to upperHalf
+        if(upperHalf.size() > lowerHalf.size()){ // If an element added above made upperHalf have one more element than lowerHalf then we poll it and put it into lowerHalf
+            lowerHalf.add(upperHalf.poll());
+        } else if(lowerHalf.size() > upperHalf.size() + 1){
+            // If an element added above, made lowerHalf have 2 more elements then upperHalf then we put one into upperHalf from lowerHalf
+            upperHalf.add(lowerHalf.poll());
+        }
     }
     
     public double findMedian() {
-        double ans = 0;
-        int n = list.size();
-        
-        if(n%2 == 1){
-            return (double)list.get((n+1)/2 - 1);
+        if(lowerHalf.size() == upperHalf.size()){
+            return (double)(lowerHalf.peek() + upperHalf.peek())/2;
+        }else{
+             return (double)(lowerHalf.peek());
         }
-        
-        int num1 = list.get(n/2 - 1);
-        int num2 = list.get(n/2);
-        
-        return (double)(num1 + num2)/2;
     }
 }
 
