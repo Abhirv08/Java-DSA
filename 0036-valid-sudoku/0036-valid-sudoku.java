d22b0fff4f4e1ced7fb499470246a9e9b2c8cc6d
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-11-23 18:33:28 +0530

    Time: 28 ms (27.17%), Space: 48.1 MB (29.28%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,54 +1,18 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        boolean c1 = validateRowsAndCols(board);
-        boolean c2 = validateSubBoxes(board);
-            
-        return c1&&c2;
-    }
-    
-    private boolean validateSubBoxes(char[][] board){        
-        
-        
-        boolean ans = true;
-        for(int row = 0; row < 9; row += 3){
-            for(int col = 0; col < 9; col += 3){
-                ans = ans&&validateSubBox(board, row, col);
-            }
-        }
-        
-        return ans;
-    }
-    
-    private boolean validateSubBox(char[][] board, int row, int col){
-        HashSet<Character> set = new HashSet<>();
-        
-        for(int i = row; i < row + 3; i++){
-            for(int j = col; j < col + 3; j++){
-                if(board[i][j] != '.'){
-                    if(!set.add(board[i][j])){
-                        return false;
-                    }
+        HashSet seen = new HashSet();
+        for(int row = 0; row < 9; row++){
+            for(int col = 0; col < 9; col++){
+                if(board[row][col] != '.'){
+                    if(!seen.add("row"+row+board[row][col]) || !seen.add("col"+col+board[row][col])){
+                    return false;
+                }
+                if(!seen.add("Box" + (row/3)*3+col/3 + board[row][col])){
+                    return false;
+                }
                 }
             }
         }
-        
-        return true;
-    }
-    
-    private boolean validateRowsAndCols(char[][] board){
-        
-        HashSet<String> set = new HashSet<>();
-        for(int i = 0; i < 9; i++){
-            for(int j = 0; j < 9; j++){
-                if(board[i][j] != '.'){
-                    if(!set.add("row"+i+board[i][j]) || !set.add("col"+j+board[i][j])){
-                        return false;
-                    }
-                    
-                }
-            }
-        }
-        
         return true;
     }
 }
