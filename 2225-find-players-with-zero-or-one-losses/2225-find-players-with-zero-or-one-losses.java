440789f79a0ca3dad667ab70b144c9987f8b037b
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-11-28 09:54:17 +0530

    Time: 499 ms (13.20%), Space: 145.7 MB (70.96%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -1,22 +1,20 @@
 class Solution {
     public List<List<Integer>> findWinners(int[][] matches) {
         int n = matches.length;
-        HashMap<Integer, Integer> losers = new HashMap<>();
-        HashMap<Integer, Integer> winners = new HashMap<>();
+        TreeMap<Integer, Integer> losers = new TreeMap<>();
+        TreeSet<Integer> winners = new TreeSet<>();
         
         for(int[] match: matches){
             losers.put(match[1], losers.getOrDefault(match[1], 0) + 1);
-            winners.put(match[0], winners.getOrDefault(match[0], 0) + 1);
+            winners.add(match[0]);
         }
         
         List<Integer> alwaysWinners = new ArrayList<>();
-        for(int player: winners.keySet()){
+        for(int player: winners){
             if(!losers.containsKey(player)){
                 alwaysWinners.add(player);
             }
         }
-        
-        Collections.sort(alwaysWinners);
         
         List<Integer> singleTimeLosers = new ArrayList<>();
         for(int player: losers.keySet()){
@@ -24,8 +22,6 @@ class Solution {
                 singleTimeLosers.add(player);
             }
         }
-        
-        Collections.sort(singleTimeLosers);
         
         List<List<Integer>> ans = new ArrayList<>();
         ans.add(alwaysWinners);
