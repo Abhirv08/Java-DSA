ff0564011fa42332a40b893e1a245f54f97d19f4
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-11-30 11:24:33 +0530

    Added solution - LeetHub

---
+++ Reorder List - GFG/reorder-list.java
@@ -0,0 +1,114 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+  public class ReorderList {
+    Node head; // head of lisl
+    Node last; // last of linked list
+
+    /* Linked list Node*/
+
+    /* Utility functions */
+
+    /* Inserts a new Node at front of the list. */
+    public void addToTheLast(Node node) {
+
+        if (head == null) {
+            head = node;
+            last = head;
+        } else {
+            // Node temp = head;
+            // while (temp.next != null) temp = temp.next;
+
+            last.next = node;
+            last = last.next;
+        }
+    }
+    /* Function to print linked list */
+    void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    /* Drier program to test above functions */
+    public static void main(String args[]) {
+
+        /* Constructed Linked List is 1->2->3->4->5->6->
+           7->8->8->9->null */
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            int n = sc.nextInt();
+            ReorderList llist = new ReorderList();
+
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) {
+                int a = sc.nextInt();
+                llist.addToTheLast(new Node(a));
+            }
+
+            llist.head = new Solution().reorderlist(llist.head);
+
+            llist.printList();
+
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/* Following is the Linked list node structure */
+
+/*class Node
+{
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}*/
+
+class Solution {
+    Node reorderlist(Node head) {
+        ArrayList<Integer> list = new ArrayList<>();
+        while(head != null){
+            list.add(head.data);
+            head = head.next;
+        }
+        
+        int i = 0, j = list.size() - 1;
+        
+        Node temp = new Node(0);
+        head = temp;
+        while(i <= j){
+            temp.next = new Node(list.get(i));
+            temp = temp.next;
+            
+            if(i != j){
+                temp.next = new Node(list.get(j));
+                temp = temp.next;
+            }
+            
+            i++;
+            j--;
+        }
+        
+        return head.next;
+    }
+}
