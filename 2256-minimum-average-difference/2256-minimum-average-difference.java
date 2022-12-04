130e373ca6e298deed912be9c19385347dc07e6e
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-12-04 12:02:06 +0530

    Time: 17 ms (95.63%), Space: 61.3 MB (93.67%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.java
+++ 2256-minimum-average-difference/2256-minimum-average-difference.java
@@ -1,40 +1,34 @@
-
 class Solution {
     public int minimumAverageDifference(int[] nums) {
         int n = nums.length;
-        int ans = -1;
-        int minAvgDiff = Integer.MAX_VALUE;
-        long currPrefixSum = 0;
         
-        // Get total sum of array.
+        long prefixSum = 0;
         long totalSum = 0;
-        for (int index = 0; index < n; ++index) {
-            totalSum += nums[index];
+        for(int num: nums){
+            totalSum += num;
         }
         
-        for (int index = 0; index < n; ++index) {
-            currPrefixSum += nums[index];
+        int minAvgDiff = Integer.MAX_VALUE;
+        int ans = -1;
+        
+        for(int i = 0; i < n; i++){
+            prefixSum += nums[i];
+            long leftAvg = prefixSum;
+            leftAvg /= (i+1);
             
-            // Calculate average of left part of array, index 0 to i.
-            long leftPartAverage = currPrefixSum;
-            leftPartAverage /= (index + 1);
-            
-            // Calculate average of right part of array, index i+1 to n-1.
-            long rightPartAverage = totalSum - currPrefixSum;
-            // If right part have 0 elements, then we don't divide by 0.
-            if (index != n - 1) {
-                rightPartAverage /= (n - index - 1);
+            long rightAvg = (totalSum - prefixSum);
+            if(i != n-1){
+                rightAvg /= (n-1-i);
             }
             
-            int currDifference = (int) Math.abs(leftPartAverage - rightPartAverage);
-            // If current difference of averages is smaller,
-            // then current index can be our answer.
-            if (currDifference < minAvgDiff) {
-                minAvgDiff = currDifference;
-                ans = index;
+            int currDiff = (int)Math.abs(leftAvg - rightAvg);
+            if(currDiff < minAvgDiff){
+                minAvgDiff = currDiff;
+                ans = i;
             }
         }
         
         return ans;
     }
 }
+
