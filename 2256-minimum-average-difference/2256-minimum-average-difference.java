de1826244d8e51c649f80aaa3f85b5c482a49727
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-12-04 11:59:14 +0530

    Time: 25 ms (63.54%), Space: 75.6 MB (51.53%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.java
+++ 2256-minimum-average-difference/2256-minimum-average-difference.java
@@ -1,66 +1,26 @@
-// class Solution {
-//     public int minimumAverageDifference(int[] nums) {
-//         int n = nums.length;
-        
-//         int[] prefixSum = new int[n];
-//         prefixSum[0] = nums[0];
-//         for(int i = 1; i < n; i++){
-//             prefixSum[i] = prefixSum[i-1] + nums[i];
-//         }
-        
-//         int[] suffixSum = new int[n];
-//         suffixSum[n-1] = 0;
-//         for(int i = n - 2; i >= 0; i--){
-//             suffixSum[i] = suffixSum[i+1] + nums[i];
-//         }
-        
-//         int minDiff = Integer.MAX_VALUE;
-//         int ans = 0;
-        
-//         for(int i = 0; i < n; i++){
-//             long prevAvg = prefixSum[i]/(i+1);
-//             long nextAvg = 0;
-//             if(n-1-i > 0){
-//                 nextAvg = suffixSum[i]/(n-1-i);
-//             }
-            
-//             int absDiff = (int)Math.abs(prevAvg - nextAvg);
-
-//             if(absDiff < minDiff){
-//                 minDiff = absDiff;
-//                 ans = i;
-//             }
-//         }
-        
-//         return ans;
-//     }
-// }
 
 class Solution {
     public int minimumAverageDifference(int[] nums) {
         int n = nums.length;
         int ans = -1;
         int minAvgDiff = Integer.MAX_VALUE;
+        long currPrefixSum = 0;
         
-        // Generate prefix and suffix sum arrays.
-        long[] prefixSum = new long[n + 1];
-        long[] suffixSum = new long[n + 1];
-        
+        // Get total sum of array.
+        long totalSum = 0;
         for (int index = 0; index < n; ++index) {
-            prefixSum[index + 1] = prefixSum[index] + nums[index];
-        }
-        
-        for (int index = n - 1; index >= 0; --index) {
-            suffixSum[index] = suffixSum[index + 1] + nums[index];
+            totalSum += nums[index];
         }
         
         for (int index = 0; index < n; ++index) {
+            currPrefixSum += nums[index];
+            
             // Calculate average of left part of array, index 0 to i.
-            long leftPartAverage = prefixSum[index + 1];
+            long leftPartAverage = currPrefixSum;
             leftPartAverage /= (index + 1);
             
             // Calculate average of right part of array, index i+1 to n-1.
-            long rightPartAverage = suffixSum[index + 1];
+            long rightPartAverage = totalSum - currPrefixSum;
             // If right part have 0 elements, then we don't divide by 0.
             if (index != n - 1) {
                 rightPartAverage /= (n - index - 1);
