2da3d2677a169894254aaad017d412915c790693
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-12-12 19:03:48 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (7.82%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n <= 2) return n;
+        int[] pp = new int[n];
+        pp[0] = 1;
+        pp[1] = 2;
+        int i = 2;
+        while(i < n){
+            pp[i] = pp[i-1]+pp[i-2];
+            i++;
+        }
+        return pp[n-1];
+    }
+}
