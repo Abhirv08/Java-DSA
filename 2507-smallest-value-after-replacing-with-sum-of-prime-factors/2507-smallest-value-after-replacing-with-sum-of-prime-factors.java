c511287466f15d5175824d7f30c336ae4ace8ce5
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-12-20 15:59:39 +0530

    Time: 5 ms (54.37%), Space: 41.2 MB (57.16%) - LeetHub

--- 2507-smallest-value-after-replacing-with-sum-of-prime-factors/2507-smallest-value-after-replacing-with-sum-of-prime-factors.java
+++ 2507-smallest-value-after-replacing-with-sum-of-prime-factors/2507-smallest-value-after-replacing-with-sum-of-prime-factors.java
@@ -1,12 +1,7 @@
 class Solution {
     public int smallestValue(int n) {
-        int[] primes = getPrimes(n);
-        
-        if(primes[n] != -1){
-            return n;
-        }
-        
-        while(primes[n] == -1){
+        int sum = n;
+        while(true){
             int temp = 0;
             
             for(int i = 2; n > 1; ){
@@ -15,39 +10,20 @@ class Solution {
                     n /= i;
                 }else{
                     i++;
-                    while(i < primes.length && primes[i] == -1){
-                        i++;
-                    }
                 }
             }
+            if(sum == temp){
+                break;
+            }
             n = temp;
-            if(n == 4) break;
-        }
-        
-        return n;
-    }
-    
-    private int[] getPrimes(int n){
-        int[] primes = new int[n+1];
-        for(int i = 1; i < primes.length; i++){
-            primes[i] = i;
-        }
-        
-        primes[0] = -1;
-        primes[1] = -1;
-        
-        int i = 2;
-        while(i < primes.length){
-            for(int j = 2*i; j < primes.length; j += i){
-                primes[j] = -1;
-            }
             
-            i++;
-            while(i < primes.length && primes[i] == -1){
-                i++;
+            if(sum == n){
+                break;
+            }else{
+                sum = n;
             }
         }
         
-        return primes;
+        return sum;
     }
 }
