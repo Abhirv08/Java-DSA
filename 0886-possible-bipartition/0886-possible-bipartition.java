4547d0f296cd4a52fbf7408a8f95d1eaf4e83823
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2022-12-21 16:59:05 +0530

    Time: 67 ms (37.28%), Space: 73.2 MB (45.18%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.java
+++ 0886-possible-bipartition/0886-possible-bipartition.java
@@ -1,48 +1,61 @@
 class Solution {
+    boolean overlapping;
+    Set<Integer> list1;
+    Set<Integer> list2;
     public boolean possibleBipartition(int n, int[][] dislikes) {
-        List<List<Integer>> adj = new ArrayList<>();
-        for(int i = 0; i < n+1; i++){
+        ArrayList<ArrayList<Integer>> adj = constructGraph(dislikes, n);
+        overlapping = false;
+        
+        boolean[] vis = new boolean[n+1];
+        list1 = new HashSet<>();
+        list2 = new HashSet<>();
+        
+        boolean ans = true;
+        for(int i = 1; i < n+1; i++){
+            if(!vis[i]){
+                list1.add(i);
+                splitIntoTwo(adj, vis, 1, i);
+            }
+        }
+        
+        if(list1.size() + list2.size() == n){
+            return true;
+        }
+        
+        return false;
+    }
+    
+    private void splitIntoTwo(ArrayList<ArrayList<Integer>> adj, boolean[] vis, int list, int node){
+        vis[node] = true;
+        
+        for(int notLiked: adj.get(node)){
+            if(list == 1){
+                list2.add(notLiked);
+            }else{
+                list1.add(notLiked);
+            }
+            if(!vis[notLiked]){
+                splitIntoTwo(adj, vis, 1 - list, notLiked);
+            }
+        }
+        
+        return ;
+    }
+    
+    private ArrayList<ArrayList<Integer>> constructGraph(int[][] dislikes, int n){
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        
+        for(int i = 0; i < n + 1; i++){
             adj.add(new ArrayList<>());
         }
         
-        for(int[] dislike: dislikes){
+        for(int[] dislike : dislikes){
             int a = dislike[0];
             int b = dislike[1];
             adj.get(a).add(b);
             adj.get(b).add(a);
         }
         
-        int[] colors = new int[n+1];
-        Arrays.fill(colors, -1);
-        
-        for(int i = 1; i < n + 1; i++){
-            if(colors[i] == -1){
-                if(!bfs(colors, i, adj)) return false;
-            }
-        }
-        
-        return true;
-    }
-    
-    boolean bfs(int[] colors, int src, List<List<Integer>> adj){
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(src);
-        colors[src] = 0;
-        
-        while(!q.isEmpty()){
-            int node = q.poll();            
-            
-            for(int neigh: adj.get(node)){
-                if(colors[neigh] == colors[node]){
-                    return false;
-                }
-                if(colors[neigh] == -1){
-                    colors[neigh] = 1 - colors[node];
-                    q.add(neigh);
-                }
-            }
-        }
-        
-        return true;
+        return adj;
     }
 }
