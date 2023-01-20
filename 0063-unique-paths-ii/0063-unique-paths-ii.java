1851218296bb1a40fef47c19d366621b5e0ff718
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2023-01-20 15:15:13 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (43.01%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,22 +1,32 @@
 class Solution {
-    int[][] dp;
+    
     public int uniquePathsWithObstacles(int[][] grid) {
         int m = grid.length, n = grid[0].length;
         if(grid[m-1][n-1] == 1) return 0;
-        dp = new int[m][n];
-        for(int[] rows: dp){
-            Arrays.fill(rows, -1);
+        int[][] dp = new int[m][n];
+        
+        for(int col = 0; col < n; col++){
+            if(grid[0][col] == 1){
+                break;
+            }
+            dp[0][col] = 1;
         }
-        return findUniquePaths(grid, m, n, 0, 0);
-    }
-    
-    private int findUniquePaths(int[][] grid, int m, int n, int row, int col){
-        if(row == m-1 && col == n-1) return 1;
-        if(row >= m || col >= n) return 0;
-        if(grid[row][col] == 1) return 0;
         
-        if(dp[row][col] != -1) return dp[row][col];
+        for(int row = 0; row < m; row++){
+            if(grid[row][0] == 1){
+                break;
+            }
+            dp[row][0] = 1;
+        }
         
-        return dp[row][col] = findUniquePaths(grid, m, n, row+1, col) + findUniquePaths(grid, m, n, row, col+1);
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(grid[i][j] != 1){
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+        
+        return dp[m-1][n-1];
     }
 }
