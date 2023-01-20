d01aa5b31034466ac32bfe6c02513a8f01567e56
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2023-01-20 19:43:09 +0530

    Time: 3 ms (73.97%), Space: 41.9 MB (77.14%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -2,24 +2,25 @@ class Solution {
     
     public int minimumTotal(List<List<Integer>> triangle) {
         int m = triangle.size();
-        int[][] dp = new int[m][m];
-        for(int[] row: dp) Arrays.fill(row, 1000000);
+        int[] prev = new int[m], curr = new int[m];
+        Arrays.fill(curr, 1000000);
         for(int i = 0; i < m; i++){
             for(int j = 0; j < i+1; j++){
-                if(i == 0 && j == 0) dp[i][j] = triangle.get(0).get(0);
+                if(i == 0 && j == 0) curr[j] = triangle.get(0).get(0);
                 else if(i > 0){
                     int up = 1000000, left = 1000000;
-                    if(i > 0) up = dp[i-1][j];
-                    if(j > 0) left = dp[i-1][j-1];
+                    if(i > 0) up = prev[j];
+                    if(j > 0) left = prev[j-1];
                     
-                    dp[i][j] = triangle.get(i).get(j) + Math.min(up, left);
+                    curr[j] = triangle.get(i).get(j) + Math.min(up, left);
                 }
             }
+            prev = Arrays.copyOf(curr, m);
         }
         
         int ans = Integer.MAX_VALUE;
         for(int i = 0; i < m; i++){
-            ans = Math.min(ans, dp[m-1][i]);
+            ans = Math.min(ans, curr[i]);
         }
         
         return ans;
