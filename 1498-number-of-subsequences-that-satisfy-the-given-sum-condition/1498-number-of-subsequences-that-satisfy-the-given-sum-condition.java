75a4d6dd521e80b20e60697c0551f8755975efc0
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2023-01-26 16:17:57 +0530

    Time: 54 ms (20.17%), Space: 71.9 MB (35.23%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.java
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -11,28 +11,16 @@ class Solution {
         }
 
         long ans = 0;
+        int r = n - 1;
+        
         for(int i = 0; i < n; i++){
-            int countMax = binarySearch(nums, target - nums[i]);
-            if(countMax < i) continue;
-            else{
-                ans = (long)((ans + pow[countMax - i])%mod);
+            while(r >= 0 && nums[i] + nums[r] > target && i <= r) r--;
+            
+            if(i <= r){
+                ans = (ans + pow[r-i])%mod;
             }
         }
         
         return (int)(ans%mod);
-    }
-    
-    int binarySearch(int[] nums, int target){
-         int start = 0, end = nums.length - 1;
-        while(start <= end){
-            int mid = start + (end - start)/2;
-            
-            if(nums[mid] <= target){
-                start = mid + 1;
-            }else{
-                end = mid - 1;
-            }
-        }
-        return end;
     }
 }
