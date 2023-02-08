a90d87c58b599200e5861a113d911f9f677f4bc0
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2023-02-08 19:01:02 +0530

    Time: 82 ms (19.69%), Space: 42.3 MB (93.90%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,25 +1,20 @@
 class Solution {
     static public int jump(int[] nums) {
         int n = nums.length;
-
+        if(n == 1) return 0;
         int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-
-        return findSteps(n, nums, 0, dp);
-    }
-
-    static int findSteps(int n, int[] nums, int idx, int[] dp){
-        if(idx == n-1){
-            return 0;
+        Arrays.fill(dp, 100001);
+        for(int i = n-2; i >= 0; i--){
+            for(int j = i + 1; j < n && j <= i + nums[i]; j++){
+                if(j == n-1){
+                    dp[i] = 1;
+                    continue;
+                }
+                int moreSteps = 1 + dp[j];
+                dp[i] = Math.min(dp[i], moreSteps);
+            }
         }
 
-        if(dp[idx] != -1) return dp[idx];
-        int ans = 100001;
-        for(int i = idx + 1; i < n && i <= idx + nums[idx]; i++){
-                int moreSteps = 1 + findSteps(n, nums, i, dp);
-                ans = Math.min(ans, moreSteps);
-        }
-
-        return dp[idx] = ans;
+        return dp[0];
     }
 }
