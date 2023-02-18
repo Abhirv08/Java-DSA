41edae425b767ce38dd7043cdc9dc56138988c1c
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2023-02-18 10:31:06 +0530

    Time: 88 ms (26.67%), Space: 99 MB (89.59%) - LeetHub

--- 2564-substring-xor-queries/2564-substring-xor-queries.java
+++ 2564-substring-xor-queries/2564-substring-xor-queries.java
@@ -1,32 +1,41 @@
 class Solution {
     public int[][] substringXorQueries(String s, int[][] queries) {
         int n = queries.length;
-            HashMap<Integer, int[]> map = new HashMap<>();
-
-            for(int i = 0; i < s.length(); i++){
-                if(s.charAt(i) == '0'){
-                    if(!map.containsKey(0)) map.put(0, new int[]{i, i});
-                    continue;
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        
+        for(int i = 0; i < n; i++){
+            int[] q = queries[i];
+            map.putIfAbsent(q[0]^q[1], new ArrayList<>());
+            map.get(q[0]^q[1]).add(i);
+        }
+        
+        int[][] ans = new int[n][2];
+        for(int[] row: ans) Arrays.fill(row, -1);
+        
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '0'){
+                if(map.containsKey(0)){
+                    updateAns(ans, map.get(0), i, i);
                 }
-                for(int j = i; j < Math.min(i+30, s.length()); j++){
-                    int num = Integer.parseInt(s.substring(i, j+1), 2);
-                    if(!map.containsKey(num)){
-                        map.put(num, new int[]{i, j});
-                    }
+                map.remove(0);
+                continue;
+            }
+            for(int j = i; j < Math.min(i+30, s.length()); j++){
+                int num = Integer.parseInt(s.substring(i, j+1), 2);
+                if(map.containsKey(num)){
+                    updateAns(ans, map.get(num), i, j);
+                    map.remove(num);
                 }
             }
-
-            int[][] ans = new int[n][2];
-            for(int[] row: ans) Arrays.fill(row, -1);
-
-            for(int i = 0; i < n; i++){
-                int[] q = queries[i];
-                int key = q[0]^q[1];
-                if (map.containsKey(key)){
-                    ans[i] = map.get(key);
-                }
-            }
-
-            return ans;
+        }
+        
+        return ans;
+    }
+    
+    void updateAns(int[][] ans, List<Integer> list, int left, int right){
+        for(int idx: list){
+            ans[idx][0] = left;
+            ans[idx][1] = right;
+        }
     }
 }
