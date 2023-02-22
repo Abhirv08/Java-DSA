cf09dc343aa11c88ba78981102678fdaabb4bdfc
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2023-02-22 19:26:23 +0530

    Time: 10 ms (62.60%), Space: 54.3 MB (8.19%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,41 +1,39 @@
 class Solution {
-    public int shipWithinDays(int[] wt, int days) {
-        int max = 0;
-            int sum = 0;
-            for(int i = 0; i < wt.length; i++){
-                if(wt[i] > max){
-                    max = wt[i];
-                }
-                sum += wt[i];
-            }
-        int l = max;
-        int r = sum;
-        while(l < r){
-            int mid = (l+r)/2;
-            if(isOk(mid, wt, days)){
-                r = mid;
-            }else{
-                l = mid+1;
-            }
+    public int shipWithinDays(int[] weights, int days) {
+        int totalLoad = 0, maxLoad = 0;
+        
+        for(int weight: weights){
+            totalLoad += weight;
+            maxLoad = Math.max(maxLoad, weight);
         }
-        return l;
-    }
-    
-    static boolean isOk(int mid, int[] weight, int days){
-        int noOfDays = 1;
-        int totalWeight = 0;
-        for(int i = 0; i < weight.length; i++){
-            if(weight[i] > mid){
-                return false;
-            }
-            if(weight[i]+totalWeight <= mid){
-                totalWeight += weight[i];
+        
+        int l = maxLoad, r = totalLoad;
+        while(l < r){
+            int m = (r + l)/2;            
+            
+            if(!findDaysToShip(weights, m, days)){
+                l = m + 1;
             }else{
-                noOfDays++;
-                totalWeight = weight[i];
+                r = m;
             }
         }
         
-        return noOfDays <= days;
+        return l;
+    }
+    
+    private boolean findDaysToShip(int[] weights, int weight, int days){
+        int daysNeeded = 1;
+        int n = weights.length;       
+        
+        int currWeight = 0;
+        for(int i = 0; i < n; i++){
+            currWeight += weights[i];
+            if(currWeight > weight){
+                currWeight = weights[i];
+                daysNeeded++;
+            }
+        }
+        
+        return daysNeeded <= days;
     }
 }
