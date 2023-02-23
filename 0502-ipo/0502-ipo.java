3c936407870b0876663ddf951f93e45ae6caf5a2
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2023-02-23 19:02:25 +0530

    Time: 124 ms (34.72%), Space: 111.3 MB (5.12%) - LeetHub

--- 0502-ipo/0502-ipo.java
+++ 0502-ipo/0502-ipo.java
@@ -1,28 +1,29 @@
 class Solution {
     public int findMaximizedCapital(int k, int w, int[] profits, int[] capital) {
-        int n = capital.length;
-        int[][] project = new int[n][2];
+        int n = profits.length;
+        
+        int[][] cmbnd = new int[n][2];
         
         for(int i = 0; i < n; i++){
-            project[i][0] = capital[i];
-            project[i][1] = profits[i];
+            cmbnd[i][0] = capital[i];
+            cmbnd[i][1] = profits[i];
         }
         
-        Arrays.sort(project, (a,b) -> a[0] == b[0] ? Integer.compare(b[1], a[1]) : Integer.compare(a[0], b[0]));
+        Arrays.sort(cmbnd, (a,b) -> Integer.compare(a[0], b[0]));
         
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] == b[1] ? Integer.compare(a[0], b[0]) : Integer.compare(b[1], a[1]));
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        if(cmbnd[0][0] > w) return 0;
         
         int i = 0;
-        while(k-- > 0 ){
-            while(i < n && project[i][0] <= w){
-                pq.add(project[i]);
+        while(k-- > 0){
+            while(i < n && cmbnd[i][0] <= w){
+                pq.add(cmbnd[i][1]);
                 i++;
             }
-            if(pq.isEmpty()){
-                return w;
-            }
-            w += pq.peek()[1];
-            pq.poll();
+            
+            if(pq.isEmpty()) break;
+            else w += pq.poll();
         }
         
         return w;
