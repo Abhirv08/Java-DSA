77f1c6fc1c7fb282fb6b2eb1d05920156d53c03c
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2023-03-08 02:03:12 +0530

    Time: 42 ms (21.67%), Space: 43.1 MB (22.41%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -6,11 +6,11 @@ class Solution {
         int[][] dp = new int[n][amount+1];
         for(int[] r: dp) Arrays.fill(r, -1);
         int minCoins = findCoins(coins, amount, n - 1, dp);
-        return minCoins == Integer.MAX_VALUE ? -1 : minCoins;
+        return minCoins == Integer.MAX_VALUE - 1 ? -1 : minCoins;
     }
 
     private static int findCoins(int[] coins, int amount, int idx, int[][] dp){
-        if(idx < 0) return Integer.MAX_VALUE;
+        if(idx < 0) return Integer.MAX_VALUE - 1;
 
         if(amount == coins[idx]) return 1;
         
@@ -18,8 +18,7 @@ class Solution {
         int notTaken = findCoins(coins, amount, idx - 1, dp);
         int taken = Integer.MAX_VALUE;
         if(amount >= coins[idx]){
-            int coinsFromNext = findCoins(coins, amount - coins[idx], idx, dp);
-            if (coinsFromNext != Integer.MAX_VALUE) taken = 1 + coinsFromNext;
+            taken = 1 + findCoins(coins, amount - coins[idx], idx, dp);
         }
 
         return dp[idx][amount] = Math.min(notTaken, taken);
