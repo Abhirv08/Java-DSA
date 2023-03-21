7483a5f3c48e1254cd44f1f0941c74b8e8c73e90
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2023-03-21 18:39:34 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (8.41%) - LeetHub

---
+++ 0517-super-washing-machines/0517-super-washing-machines.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findMinMoves(int[] machines) {
+        int n = machines.length;
+        int clothes = 0;
+        for(int num: machines) clothes += num;
+        
+        if(clothes % n != 0) return -1;
+        
+        int req = clothes/n;
+        int clothes_so_far = 0, max = 0;
+        
+        for(int pres: machines){
+            clothes_so_far += pres - req;
+            
+            max = Math.max(max, Math.abs(clothes_so_far));
+            max = Math.max(max, pres - req);
+        }
+        
+        return max;
+    }
+}
