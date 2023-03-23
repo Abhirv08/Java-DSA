5b1f0a97881779ae801c679e2f69ef9ac355471b
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2023-03-24 00:53:48 +0530

    Time: 24 ms (17.25%), Space: 49.6 MB (45.10%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -4,41 +4,20 @@ class Solution {
         
         int[][] dp = new int[s_len + 1][t_len + 1];
         
-//         for(int i = 1; i <= s_len; i++){
-//             for(int j = 1; j <= t_len; j++){
-//                 if(s.charAt(i - 1) == t.charAt(j - 1)){
-//                     dp[i][j] += dp[i - 1][j - 1];
-//                 }
+        for(int i = 0; i <= s_len; i++){
+            dp[i][0] = 1;
+        }
+        
+        for(int i = 1; i <= s_len; i++){
+            for(int j = 1; j <= t_len; j++){
+                if(s.charAt(i - 1) == t.charAt(j - 1)){
+                    dp[i][j] += dp[i - 1][j - 1];
+                }
 
-//                 dp[i][j] += dp[i - 1][j];
-//             }
-//         }
-        
-//         return dp[s_len][t_len];
-        
-        for(int[] r: dp) Arrays.fill(r, -1);
-        
-        return countSubSeq(s, s_len-1, t, t_len-1, dp);
-    }
-    
-    private int countSubSeq(String s, int i1, String t, int i2, int[][] dp){
-        if(i2 == -1){
-            return 1;
+                dp[i][j] += dp[i - 1][j];
+            }
         }
         
-        if(i1 == -1){
-            return 0;
-        }
-        
-        if(dp[i1+1][i2+1] != -1) return dp[i1+1][i2+1];
-        
-        int take = 0;
-        if(s.charAt(i1) == t.charAt(i2)){
-            take = countSubSeq(s, i1-1, t, i2-1, dp);
-        }
-        
-        int notTake = countSubSeq(s, i1-1, t, i2, dp);
-        
-        return dp[i1+1][i2+1] = take + notTake;
+        return dp[s_len][t_len];
     }
 }
