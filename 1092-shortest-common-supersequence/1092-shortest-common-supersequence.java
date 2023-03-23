aace032cba918deb4ccf4d37cd73bd0b2c8557b8
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2023-03-23 23:53:54 +0530

    Time: 14 ms (85.81%), Space: 46.3 MB (42.97%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -1,50 +1,48 @@
 class Solution {
-    public String shortestCommonSupersequence(String s1, String s2) {
-        int s1_l = s1.length();
-        int s2_l = s2.length();
+    public String shortestCommonSupersequence(String str1, String str2) {
+        int m = str1.length();
+        int n = str2.length();
         
-        int[][] dp = new int[s1_l + 1][s2_l + 1];
+        int[][] dp = new int[m + 1][n + 1];
         
-        for(int i = 1; i < s1_l + 1; i++){
-            for(int j = 1; j < s2_l + 1; j++){
-                if(s1.charAt(i - 1) == s2.charAt(j - 1)){
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(str1.charAt(i - 1) == str2.charAt(j - 1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
                 }else{
-                    dp[i][j] = Math.max( dp[i - 1][j], dp[i][j - 1] );
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
         
-        int i = s1_l;
-        int j = s2_l;
-        
         StringBuilder ans = new StringBuilder();
         
+        int i = m, j = n;
+        
         while(i > 0 && j > 0){
-            if(s1.charAt(i - 1) == s2.charAt(j - 1) ){
-                ans.insert(0, s1.charAt(i - 1));
-                i--; j--;
-            }else{
-                if(dp[i - 1][j] > dp[i][j - 1]){
-                    ans.insert(0, s1.charAt(i - 1));
-                    i--;
-                }else{
-                    ans.insert(0, s2.charAt(j - 1));
-                    j--;
-                }
+            if(str1.charAt(i-1) == str2.charAt(j-1)){
+                ans.append(str1.charAt(i-1));
+                i--;
+                j--;
+            }else if(dp[i-1][j] >= dp[i][j-1]){
+                ans.append(str1.charAt(i-1));
+                i--;
+            }else if(dp[i-1][j] < dp[i][j-1]){
+                ans.append(str2.charAt(j-1));
+                j--;
             }
         }
         
         while(i > 0){
-            ans.insert(0, s1.charAt(i - 1));
+            ans.append(str1.charAt(i - 1));
             i--;
         }
         
         while(j > 0){
-            ans.insert(0, s2.charAt(j - 1));
+            ans.append(str2.charAt(j - 1));
             j--;
         }
         
-        return ans.toString();
+        return ans.reverse().toString();
     }
 }
