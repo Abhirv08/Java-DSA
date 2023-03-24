72acb0bebe007fb5b297ab9fdb650da47121bfbb
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2023-03-24 12:40:54 +0530

    Time: 4 ms (88.99%), Space: 42.6 MB (58.65%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,32 +1,32 @@
 class Solution {
-    Integer memo[][];
-
     public int minDistance(String word1, String word2) {
-        memo = new Integer[word1.length() + 1][word2.length() + 1];
-        return minDistanceRecur(word1, word2, word1.length(), word2.length());
+        int m = word1.length();
+        int n = word2.length();
+        
+        int[][] dp = new int[m+1][n+1];
+        
+        for(int[] r: dp) Arrays.fill(r, -1);
+        
+        return findMinOper(word1, m-1, word2, n-1, dp);
     }
-
-    int minDistanceRecur(String word1, String word2, int word1Index, int word2Index) {
-        if (word1Index == 0) {
-            return word2Index;
+    
+    private int findMinOper(String w1, int i1, String w2, int i2, int[][] dp){
+        if(i2 < 0){
+            return i1 + 1;
         }
-        if (word2Index == 0) {
-            return word1Index;
+        
+        if(i1 < 0) return i2 + 1;
+        
+        if(dp[i1+1][i2+1] != -1) return dp[i1+1][i2+1];
+        
+        if(w1.charAt(i1) == w2.charAt(i2)){
+           return dp[i1+1][i2+1] = findMinOper(w1, i1 - 1, w2, i2 - 1, dp);
         }
-        if (memo[word1Index][word2Index] != null) {
-            return memo[word1Index][word2Index];
-        }
-        int minEditDistance = 0;
-        if (word1.charAt(word1Index - 1) == word2.charAt(word2Index - 1)) {
-            minEditDistance = minDistanceRecur(word1, word2, word1Index - 1, word2Index - 1);
-        }
-        else {
-            int insertOperation = minDistanceRecur(word1, word2, word1Index, word2Index - 1);
-            int deleteOperation = minDistanceRecur(word1, word2, word1Index - 1, word2Index);
-            int replaceOperation = minDistanceRecur(word1, word2, word1Index - 1, word2Index - 1);
-            minEditDistance = Math.min(insertOperation, Math.min(deleteOperation, replaceOperation)) + 1;
-        }
-        memo[word1Index][word2Index] = minEditDistance;
-        return minEditDistance;
+        
+        int insert = findMinOper(w1, i1, w2, i2 - 1, dp);
+        int replace = findMinOper(w1, i1 - 1, w2, i2 - 1, dp);
+        int delete = findMinOper(w1, i1 - 1, w2, i2, dp);
+        
+        return dp[i1+1][i2+1] = 1 + Math.min(insert, Math.min(replace, delete));
     }
 }
