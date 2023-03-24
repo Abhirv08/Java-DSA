8ea7cac23362a85196469e6d9ba5f07957342414
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2023-03-24 13:46:08 +0530

    Time: 7 ms (18.70%), Space: 42.6 MB (46.23%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -6,25 +6,23 @@ class Solution {
         int[][] dp = new int[m+1][n+1];
         
         for(int i = 0; i <= m; i++){
-            dp[i][0] = i;
-        }
-        
-        for(int j = 0; j <= n; j++){
-            dp[0][j] = j;
-        }
-        
-        for(int i = 1; i <= m; i++){
-            for(int j = 1; j <= n; j++){
-                if(word1.charAt(i-1) == word2.charAt(j-1)){
-                   dp[i][j] = dp[i - 1][j - 1];
-                    continue;
+            for(int j = 0; j <= n; j++){
+                if(i == 0){
+                    dp[i][j] = j;
+                }else if(j == 0){
+                    dp[i][j] = i;
+                }else{
+                    if(word1.charAt(i-1) == word2.charAt(j-1)){
+                       dp[i][j] = dp[i - 1][j - 1];
+                        continue;
+                    }
+
+                    int insert = dp[i][j-1];
+                    int replace = dp[i-1][j-1];
+                    int delete = dp[i-1][j];
+
+                    dp[i][j] = 1 + Math.min(insert, Math.min(replace, delete));
                 }
-
-                int insert = dp[i][j-1];
-                int replace = dp[i-1][j-1];
-                int delete = dp[i-1][j];
-
-                dp[i][j] = 1 + Math.min(insert, Math.min(replace, delete));
             }
         }
         
