3c1b46f77fb2da2751dfd43d1d3a679af843f296
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2023-03-29 21:43:57 +0530

    Time: 9 ms (30.15%), Space: 43 MB (14.18%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.java
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -2,21 +2,17 @@ class Solution {
     public int maxSatisfaction(int[] s) {
         Arrays.sort(s);
         int n = s.length;
-        int[][] dp = new int[n][n];
+        int[][] dp = new int[n+1][n+2];
         
-        for(int[] r: dp) Arrays.fill(r, -1);
+        for(int i = n-1; i >= 0; i--){
+            for(int c = 1; c <= n; c++){
+                int make = c*s[i] + dp[i+1][c+1];
+                int notMake = dp[i+1][c];
+
+                dp[i][c] = Math.max(make, notMake);
+            }
+        }
         
-        return maximize(s, 0, 0, dp);
-    }
-    
-    private int maximize(int[] s, int idx, int count, int[][] dp){
-        if(idx == s.length) return 0;
-        
-        if(dp[idx][count] != -1) return dp[idx][count];
-        // if chef decided to prepare
-        int make = (count+1)*s[idx] + maximize(s, idx+1, count+1, dp);
-        int notMake = maximize(s, idx+1, count, dp);
-        
-        return dp[idx][count] = Math.max(make, notMake);
+        return dp[0][1];
     }
 }
