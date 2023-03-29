dfe844e54ec7ec758d1ae531491abb9123608dd4
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2023-03-29 22:04:18 +0530

    Time: 2 ms (64.95%), Space: 40.9 MB (54.90%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.java
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -2,17 +2,28 @@ class Solution {
     public int maxSatisfaction(int[] s) {
         Arrays.sort(s);
         int n = s.length;
-        int[][] dp = new int[n+1][n+2];
         
-        for(int i = n-1; i >= 0; i--){
-            for(int c = 1; c <= n; c++){
-                int make = c*s[i] + dp[i+1][c+1];
-                int notMake = dp[i+1][c];
-
-                dp[i][c] = Math.max(make, notMake);
-            }
+        int[] prefixSum = new int[n];
+        prefixSum[n-1] = s[n-1];
+        
+        for(int i = n-2; i >= 0; i--){
+            prefixSum[i] = prefixSum[i+1] + s[i];
         }
         
-        return dp[0][1];
+        // System.out.println(Arrays.toString(prefixSum));
+        int ans = 0, curr = 0;
+        
+        for(int i = n-1; i >= 0 && curr >= ans; i--){
+            curr += 1*s[i];
+            if(i < n-1) curr += prefixSum[i+1];
+            
+            if(ans <= curr){
+                ans = curr;
+            }else{
+                break;
+            }           
+        }
+        
+        return ans;
     }
 }
