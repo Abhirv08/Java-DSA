3e9edd1b7326e02b0f17b026b488b901ce2013d6
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2023-03-31 16:18:39 +0530

    Time: 132 ms (40.63%), Space: 65.6 MB (82.38%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -2,25 +2,28 @@ class NumMatrix {
     int[][] prefixSum;
     public NumMatrix(int[][] matrix) {
         int rows = matrix.length, cols = matrix[0].length;
-        prefixSum = new int[rows][cols];
+        prefixSum = new int[rows+1][cols+1];
         
-        for(int i = 0; i < rows; i++){
-            prefixSum[i][0] = matrix[i][0];
-            for(int j = 1; j < matrix[0].length; j++){
-                prefixSum[i][j] = prefixSum[i][j-1] + matrix[i][j];
+        prefixSum[1][1] = matrix[0][0];
+        for(int j = 1; j < cols; j++){
+            prefixSum[1][j+1] = prefixSum[1][j] + matrix[0][j];
+        }
+        
+        for(int i = 1; i < rows; i++){
+            prefixSum[i+1][1] = prefixSum[i][1] + matrix[i][0];
+        }
+        
+        for(int i = 2; i < rows+1; i++){
+            for(int j = 2; j < cols+1; j++){
+                prefixSum[i][j] = prefixSum[i-1][j] + prefixSum[i][j-1] +
+                    + matrix[i-1][j-1] - prefixSum[i-1][j-1];
             }
         }
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int ans = 0;
-        for(int r = row1; r <= row2; r++){
-            if(col1 > 0){
-                ans += prefixSum[r][col2] - prefixSum[r][col1 - 1];
-            }else{
-                ans += prefixSum[r][col2];
-            }
-        }
+        row1 += 1; col1 += 1; row2 += 1; col2 += 1;
+        int ans = prefixSum[row2][col2] + prefixSum[row1-1][col1-1] - prefixSum[row1-1][col2] - prefixSum[row2][col1-1]; 
         
         return ans;
     }
