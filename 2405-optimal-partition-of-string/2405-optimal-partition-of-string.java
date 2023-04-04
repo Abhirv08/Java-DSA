6ca953dcb295c5a70ccfb00e03825d840bead94a
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2023-04-04 10:50:36 +0530

    Time: 41 ms (20.81%), Space: 43.2 MB (59.94%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
@@ -2,15 +2,15 @@ class Solution {
     public int partitionString(String s) {
         int count = 0;
         
+        HashSet<Character> set = new HashSet<>();
         int i = 0;
-        while(i < s.length()){
-            HashSet<Character> set = new HashSet<>();
-            
+        while(i < s.length()){            
             while(i < s.length() && !set.contains(s.charAt(i))){
                 set.add(s.charAt(i));
                 i++;
             }
             count++;
+            set.clear();
         }
         
         return count;
