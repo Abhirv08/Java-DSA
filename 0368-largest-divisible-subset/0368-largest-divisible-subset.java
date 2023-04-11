a7119abe9ecd3591c4e9c04b228bf5e5ed52bebb
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2023-04-12 00:33:10 +0530

    Time: 40 ms (8.65%), Space: 42.7 MB (54.07%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.java
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.java
@@ -1,38 +1,25 @@
-class Solution {
-    public List<Integer> largestDivisibleSubset(int[] nums) {
-        int n = nums.length;
-        Arrays.sort(nums);
-        int[] pre = new int[n];
-        Arrays.fill(pre, 1);
-        
-        int maxCount = 0, idx = 0;;
-        for(int i = 1; i < n; i++){
-            for(int j = 0; j < i; j++){
-                if(nums[i]%nums[j] == 0){
-                    pre[i] = Math.max(pre[i], pre[j] + 1);
-                }
-            }
-            if(pre[i] > maxCount){
-                maxCount = pre[i];
-                idx = i;
-            }
-        }
-        
-        List<Integer> list = new ArrayList<>();
-        int lastNum = nums[idx];
-        while(maxCount > 0){
-            list.add(nums[idx]);
-            maxCount--;
-            idx--;
-            while(idx >= 0 && (pre[idx] != maxCount || lastNum%nums[idx] != 0)){
-                idx--;
-            }
-            if(idx < 0) break;
-            lastNum = nums[idx];
-        }
-        
-        if(list.size() == 0) list.add(nums[0]);
-        
-        return list;
+public class Solution {
+  List<Integer> res;
+  int[] mem;
+public List<Integer> largestDivisibleSubset(int[] nums) {
+    Arrays.sort(nums);
+    mem = new int[nums.length];
+    Arrays.fill(mem, -1);
+    res = new ArrayList<>();
+    helper(nums, 0, new ArrayList<>(), 1);
+    return res;
+}
+private void helper(int[] nums, int index, List<Integer> curr, int prev) {
+    if (curr.size() > res.size()) {
+        res = new ArrayList<>(curr);
     }
+    for (int i = index; i < nums.length; i++) {
+        if (curr.size() > mem[i] && nums[i] % prev == 0) {
+            mem[i] = curr.size();
+            curr.add(nums[i]);
+            helper(nums, i + 1, curr, nums[i]);
+            curr.remove(curr.size() - 1);
+        }
+    }
+  }
 }
