94c978bbc72cd8522483be96da6a0593855f81de
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2023-04-16 18:15:27 +0530

    Time: 185 ms (12.57%), Space: 59.4 MB (18.58%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.java
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.java
@@ -1,27 +1,51 @@
 class Solution {
+    long mod = (long) 1e9+7;
+    long[][] dp;
     public int numWays(String[] words, String target) {
         int n = words[0].length();
-        int m = target.length();
-        int mod = 1000000007;
-        int[] dp = new int[m+1];
-        dp[0] = 1;
-        
-        int[][] cnt = new int[n][26];
-        
+
+        int[][] mat = new int[26][n];
+
         for(String word: words){
-            for(int i = 0; i < n; i++){
+            for(int i = 0; i < word.length(); i++){
                 char ch = word.charAt(i);
-                cnt[i][ch-'a']++;
+                mat[ch - 'a'][i]++;
             }
         }
         
-        for(int i = 0 ; i < n; i++){
-            for(int j = m - 1; j >= 0; j--){
-                dp[j+1] += (int) ((long) dp[j]*cnt[i][target.charAt(j) - 'a']%mod);
-                dp[j+1] %= mod;
+        int[][] ps = new int[26][n];
+        
+        for(int r = 0; r < 26; r++){
+            for(int c = n-1; c >= 0; c--){
+                if(c == n - 1){
+                    ps[r][c] = mat[r][c];
+                }else{
+                    ps[r][c] = ps[r][c+1] + mat[r][c];
+                }
             }
         }
         
-        return dp[m];
+        dp = new long[n][target.length()];
+        
+        for(long[] arr: dp) Arrays.fill(arr, -1);
+        
+        return (int) (findWays(ps, n, 0, target, 0)%mod);
+    }
+    
+    private long findWays(int[][] ps, int n, int idx, String target, int tgtIdx){
+        char ch = target.charAt(tgtIdx);
+        if(idx == n) return 0;
+        
+        if(tgtIdx == target.length() - 1){
+            return ps[ch - 'a'][idx];
+        }
+        
+        if(dp[idx][tgtIdx] != -1) return dp[idx][tgtIdx];
+        
+        int freq = ps[ch-'a'][idx] - (idx+1 < n ? ps[ch-'a'][idx+1] : 0);
+        long ways = (freq*findWays(ps, n, idx+1, target, tgtIdx+1)%mod)%mod;
+        ways += findWays(ps, n, idx+1, target, tgtIdx)%mod;
+        
+        return dp[idx][tgtIdx] = ways%mod;
     }
 }
