b706a78b71c471e635af1da88788a81a1d4fa735
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2023-05-15 22:33:53 +0530

    Time: 11 ms (7.78%), Space: 57.4 MB (77.49%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.java
@@ -8,66 +8,52 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class Solution {
-    public ListNode swapNodes(ListNode head, int n) {
-        if(head.next == null){
-            return head;
-        }
+    public ListNode swapNodes(ListNode head, int k) {
+        ListNode f = null;
+        ListNode l = null;
+        
         ListNode temp = head;
-        int size = 0;
+        
+        int len = getLength(head);
+        
+        int count = 1;
         while(temp != null){
-            size++;
+            if(count == k){
+                f = temp;
+            }
+            
+            if(count == len - k + 1){
+                l = temp;
+            }
+            
+            count++;
             temp = temp.next;
         }
-        if(size==2){
-            ListNode temp2 = head.next;
-            temp2.next = head;
-            head.next = null;
-            head = temp2;
-            return head;
-        }
-        int first = n-1;
-        int second = size-n; 
-        if(first > second){
-            int temp3 = first;
-            first = second;
-            second =temp3;
-        }
-        if(first==0){            
-            ListNode p2 = head;
-                while(second > 1){
-                p2 = p2.next;
-                second--;
-            }
-            ListNode firstNumToBeSwapped = head;
-            ListNode secondNumToBeSwapped = p2.next;
-            p2.next = firstNumToBeSwapped;
-            secondNumToBeSwapped.next = firstNumToBeSwapped.next;
-            firstNumToBeSwapped.next = null;
-            head = secondNumToBeSwapped;
-            
-            return head;
-        }
-        ListNode p1 = head;
-        ListNode p2 = head;
-        while(first > 1 && second > 1){
-            p1 = p1.next;  
-            p2= p2.next;
-            first--;
-            second--;
-        }
-        while(second > 1){
-            p2 = p2.next;
-            second--;
-        }
-        if(p1==p2) return head;
-        ListNode firstNumToBeSwapped = p1.next;
-        ListNode secondNumToBeSwapped = p2.next;
-        p1.next = secondNumToBeSwapped;        
-        p2.next = firstNumToBeSwapped;
-        temp = firstNumToBeSwapped.next;
-        firstNumToBeSwapped.next = secondNumToBeSwapped.next;        
-        secondNumToBeSwapped.next = temp;;
+        
+        if(f == l) return head;
+        
+        System.out.println(f.val + ":" + l.val);
+        
+        int t_v = f.val;
+        f.val = l.val;
+        l.val = t_v;
+        
         return head;
+        
+    }
+    
+    private int getLength(ListNode head){
+        int len = 0;
+        
+        ListNode temp = head;
+        
+        while(temp != null){
+            len++;
+            temp = temp.next;
+        }
+        
+        return len;
     }
 }
