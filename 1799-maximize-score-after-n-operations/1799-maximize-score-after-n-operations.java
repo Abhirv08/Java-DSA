f603f31a2fac0e73875495531428b5383b7430b5
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2023-05-16 15:32:23 +0530

    Time: 186 ms (69.12%), Space: 42.1 MB (79.02%) - LeetHub

--- 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.java
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.java
@@ -1,57 +1,32 @@
 class Solution {
-    public int backtrack(int[] nums, int mask, int pairsPicked, int[] memo) {
-        // If we have picked all the numbers from 'nums' array, we can't get more score.
-        if (2 * pairsPicked == nums.length) {
-            return 0;
-        }
-
-        // If we already solved this sub-problem then return the stored result.
-        if (memo[mask] != -1) {
-            return memo[mask];
-        }
-
-        int maxScore = 0;
-
-        // Iterate on 'nums' array to pick the first and second number of the pair.
-        for (int firstIndex = 0; firstIndex < nums.length; ++firstIndex) {
-            for (int secondIndex = firstIndex + 1; secondIndex < nums.length; ++secondIndex) {
+    int[] cache;
+    public int maxScore(int[] nums) {        
+        cache = new int[(1 << nums.length)];
+        Arrays.fill(cache, -1);
+        return maxScore(nums, 0, 1);
+    }
     
-                // If the numbers are same, or already picked, then we move to next number.
-                if (((mask >> firstIndex) & 1) == 1 || ((mask >> secondIndex) & 1) == 1) {
-                    continue;
-                }
-
-                // Both numbers are marked as picked in this new mask.
-                int newMask = mask | (1 << firstIndex) | (1 << secondIndex);
-
-                // Calculate score of current pair of numbers, and the remaining array.
-                int currScore = (pairsPicked + 1) * gcd(nums[firstIndex], nums[secondIndex]);
-                int remainingScore = backtrack(nums, newMask, pairsPicked + 1, memo);
-
-                // Store the maximum score.
-                maxScore = Math.max(maxScore, currScore + remainingScore);
-                // We will use old mask in loop's next interation, 
-                // means we discarded the picked number and backtracked.
+    private int maxScore(int[] nums, int mask, int op){   
+        if(cache[mask] != -1) return cache[mask];
+        int max = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+            if(((1 << i) & mask) > 0) continue;
+            for(int j = i + 1; j < nums.length; j++){
+                if(((1 << j) & mask) > 0) continue;
+                
+                int newMask = mask | (1 << i) | (1 << j);
+                int score = op*getGCD(nums[i], nums[j]) + maxScore(nums, newMask, op+1);
+                max = Math.max(max, score);
             }
-        }
-
-        // Store the result of the current sub-problem.
-        memo[mask] = maxScore;
-        return maxScore;
+        }        
+        
+        return cache[mask] = max;
     }
-
-    public int maxScore(int[] nums) {
-        int memoSize = 1 << nums.length; // 2^(nums array size)
-        int[] memo = new int[memoSize];
-        Arrays.fill(memo, -1);
-        return backtrack(nums, 0, 0, memo);
-    }
-
-    // Utility function to calculate the gcd of two numbers.
-    public int gcd(int a, int b) {
-        if (b == 0) {
-            return a;
-        }
-        return gcd(b, a % b);
+    
+    private int getGCD(int a, int b){
+        if(b == 0) return a;
+        
+        return getGCD(b, a%b);
     }
 }
