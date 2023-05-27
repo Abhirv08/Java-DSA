77420ea9618386c73ae649064507c9836dadf80d
Abhishek Kumar <81900610+Abhirv08@users.noreply.github.com>
2023-05-27 23:44:47 +0530

    Time: 2 ms (100.00%), Space: 43.2 MB (20.00%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.java
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -1,8 +1,17 @@
 class Solution {
     public int buyChoco(int[] prices, int money) {
-        Arrays.sort(prices);
+        int f = 101, s = 101;
         
-        int diff = money - prices[0] - prices[1];
+        for(int p: prices){
+            if(p <= f){
+                s = f;
+                f = p;
+            }else if(p <= s){
+                s = p;
+            }
+        }        
+        
+        int diff = money - s - f;
         
         if(diff < 0) return money;
         
